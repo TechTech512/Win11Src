c0de2da7cf089eb7a3cc07df4ea2e91e13584ff4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-08-02 21:21:23 -0400

    Add files via upload

---
+++ NT/onecore/base/ntsetup/migshared/pcha/pch.h
@@ -0,0 +1,31 @@
+#pragma warning(push)
+
+#include <windows.h>
+#include <wchar.h>
+#include <tchar.h>
+#include <stdlib.h>
+#include <imagehlp.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <setupapi.h>
+#include <shlobj.h>
+#include <objidl.h>
+#include <mmsystem.h>
+
+#include <setupbat.h>
+#include <sputils.h>
+
+#include "chartype.h"
+
+#define COBJMACROS
+
+//
+// Common includes
+//
+
+#include "coreutil.h"
+
+#pragma warning(pop)
+
+
