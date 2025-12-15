17e28a4078f4e17163d89241b198fce23dd89e31
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-14 21:54:49 -0500

    Add files via upload

---
+++ NT/onecore/inetcore/ext/ratings/msrating/stubs.cpp
@@ -0,0 +1,161 @@
+#include <windows.h>
+
+HRESULT WINAPI ChangeSupervisorPassword(wchar_t* param_1, wchar_t* param_2)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI ClickedOnPRF(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingAccessDeniedDialog2(HWND param_1, char* param_2, void* param_3)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingAccessDeniedDialog2W(HWND param_1, char* param_2, void* param_3)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI ClickedOnRAT(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingAccessDeniedDialog(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingAccessDeniedDialogW(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingAddPropertyPages(HWND param_1, char* param_2, void* param_3)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingAddToApprovedSites(HWND param_1, DWORD param_2, BYTE* param_3, wchar_t* param_4, int param_5, int param_6, int param_7)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCheckUserAccess(char* param_1, char* param_2, char* param_3, BYTE* param_4, DWORD param_5, void** param_6)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCheckUserAccessW(char* param_1, char* param_2, char* param_3, BYTE* param_4, DWORD param_5, void** param_6)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingClickedOnPRFInternal(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingClickedOnRATInternal(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomAddRatingHelper(char* param_1, char* param_2, char* param_3, BYTE* param_4, DWORD param_5, void** param_6)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomAddRatingSystem(wchar_t* param_1, wchar_t* param_2)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomCrackData(HWND param_1, char* param_2, void* param_3)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomDeleteCrackedData(void* param_1)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomInit(void* param_1)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomRemoveRatingHelper(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomSetDefaultBureau(void* param_1)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingCustomSetUserOptions(wchar_t* param_1, wchar_t* param_2)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingEnable(HWND param_1, char* param_2, void* param_3)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingEnableW(HWND param_1, char* param_2, void* param_3)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingEnabledQuery()
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingFreeDetails(void* param_1)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingInit()
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingObtainCancel(void* param_1)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingObtainQuery(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingObtainQueryW(HWND param_1, HINSTANCE param_2, char* param_3, int param_4)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingSetupUI(wchar_t* param_1, wchar_t* param_2)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI RatingSetupUIW(wchar_t* param_1, wchar_t* param_2)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT WINAPI VerifySupervisorPassword(void* param_1)
+{
+    return E_NOTIMPL;
+}
