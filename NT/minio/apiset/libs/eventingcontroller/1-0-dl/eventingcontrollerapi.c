74e369f0c55f6a841a3e83324d844920549b263a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 07:33:34 -0500

    Add files via upload

---
+++ NT/minio/apiset/libs/eventingcontroller/1-0-dl/eventingcontrollerapi.c
@@ -0,0 +1,59 @@
+#pragma warning (disable:4098)
+
+#include <windows.h>
+
+void ControlTraceW(void)
+{
+	return TRUE;
+}
+
+void EnableTraceEx2(void)
+{
+	return TRUE;
+}
+
+void EnumerateTraceGuidsEx(void)
+{
+	return TRUE;
+}
+
+void EventAccessControl(void)
+{
+	return TRUE;
+}
+
+void EventAccessQuery(void)
+{
+	return TRUE;
+}
+
+void EventAccessRemove(void)
+{
+	return TRUE;
+}
+
+void QueryAllTracesW(void)
+{
+	return TRUE;
+}
+
+void StartTraceW(void)
+{
+	return TRUE;
+}
+
+void StopTraceW(void)
+{
+	return TRUE;
+}
+
+void TraceQueryInformation(void)
+{
+	return TRUE;
+}
+
+void TraceSetInformation(void)
+{
+	return TRUE;
+}
+
