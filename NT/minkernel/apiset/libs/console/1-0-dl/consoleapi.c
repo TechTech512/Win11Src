94b7343a04a1ebcb9aab893668857456ae0ebac9
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 17:21:53 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/console/1-0-dl/consoleapi.c
@@ -0,0 +1,72 @@
+#pragma warning (disable:4098)
+
+void AllocConsole(void)
+{
+	return 1;
+}
+
+void GetConsoleCP(void)
+{
+	return 1;
+}
+
+void GetConsoleMode(void)
+{
+	return 1;
+}
+
+void GetConsoleOutputCP(void)
+{
+	return 1;
+}
+
+void GetNumberOfConsoleInputEvents(void)
+{
+	return 1;
+}
+
+void PeekConsoleInputA(void)
+{
+	return 1;
+}
+
+void ReadConsoleA(void)
+{
+	return 1;
+}
+
+void ReadConsoleInputA(void)
+{
+	return 1;
+}
+
+void ReadConsoleInputW(void)
+{
+	return 1;
+}
+
+void ReadConsoleW(void)
+{
+	return 1;
+}
+
+void SetConsoleCtrlHandler(void)
+{
+	return 1;
+}
+
+void SetConsoleMode(void)
+{
+	return 1;
+}
+
+void WriteConsoleA(void)
+{
+	return 1;
+}
+
+void WriteConsoleW(void)
+{
+	return 1;
+}
+
