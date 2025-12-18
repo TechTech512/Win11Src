a84864a7bd47e23030665d46184959574aefcc67
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 09:07:52 -0500

    Add files via upload

---
+++ NT/minio/apiset/libs/sddl/1-0-dl/sddl.c
@@ -0,0 +1,24 @@
+#pragma warning (disable:4098)
+
+#include <windows.h>
+
+void ConvertSecurityDescriptorToStringSecurityDescriptorW(void)
+{
+	return TRUE;
+}
+
+void ConvertSidToStringSidW(void)
+{
+	return TRUE;
+}
+
+void ConvertStringSecurityDescriptorToSecurityDescriptorW(void)
+{
+	return TRUE;
+}
+
+void ConvertStringSidToSidW(void)
+{
+	return TRUE;
+}
+
