a3a3c010e3c6c2d3b2c4a209bfd8507738f4b052
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 15:32:44 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/comm/1-0-dl/commapi.c
@@ -0,0 +1,92 @@
+#pragma warning (disable:4098)
+
+void ClearCommBreak(void)
+{
+	return 1;
+}
+
+void ClearCommError(void)
+{
+	return 1;
+}
+
+void EscapeCommFunction(void)
+{
+	return 1;
+}
+
+void GetCommConfig(void)
+{
+	return 1;
+}
+
+void GetCommMask(void)
+{
+	return 1;
+}
+
+void GetCommModemStatus(void)
+{
+	return 1;
+}
+
+void GetCommProperties(void)
+{
+	return 1;
+}
+
+void GetCommState(void)
+{
+	return 1;
+}
+
+void GetCommTimeouts(void)
+{
+	return 1;
+}
+
+void PurgeComm(void)
+{
+	return 1;
+}
+
+void SetCommBreak(void)
+{
+	return 1;
+}
+
+void SetCommConfig(void)
+{
+	return 1;
+}
+
+void SetCommMask(void)
+{
+	return 1;
+}
+
+void SetCommState(void)
+{
+	return 1;
+}
+
+void SetCommTimeouts(void)
+{
+	return 1;
+}
+
+void SetupComm(void)
+{
+	return 1;
+}
+
+void TransmitCommChar(void)
+{
+	return 1;
+}
+
+void WaitCommEvent(void)
+{
+	return 1;
+}
+
