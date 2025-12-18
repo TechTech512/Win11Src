11f097a463889f4d6dd86d7db5b8155377fb8b4a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 18:37:00 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/debug/1-0-dl/debugapi.c
@@ -0,0 +1,22 @@
+#pragma warning (disable:4098)
+
+void DebugBreak(void)
+{
+	return 1;
+}
+
+void IsDebuggerPresent(void)
+{
+	return 1;
+}
+
+void OutputDebugStringA(void)
+{
+	return 1;
+}
+
+void OutputDebugStringW(void)
+{
+	return 1;
+}
+
---
+++ NT/minkernel/apiset/libs/debug/1-1-dl/debugapi.c
@@ -0,0 +1,27 @@
+#pragma warning (disable:4098)
+
+void CheckRemoteDebuggerPresent(void)
+{
+	return 1;
+}
+
+void ContinueDebugEvent(void)
+{
+	return 1;
+}
+
+void DebugActiveProcess(void)
+{
+	return 1;
+}
+
+void DebugActiveProcessStop(void)
+{
+	return 1;
+}
+
+void WaitForDebugEvent(void)
+{
+	return 1;
+}
+
