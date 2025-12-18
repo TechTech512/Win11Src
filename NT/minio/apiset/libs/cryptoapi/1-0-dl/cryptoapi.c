233fd108d2ea5fb7c3db586d4099e14b5fcbfcc6
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 14:32:02 -0500

    Add files via upload

---
+++ NT/minio/apiset/libs/cryptoapi/1-0-dl/cryptoapi.c
@@ -0,0 +1,199 @@
+#pragma warning (disable:4098)
+
+#include <windows.h>
+
+void CryptAcquireContextA(void)
+{
+	return TRUE;
+}
+
+void CryptAcquireContextW(void)
+{
+	return TRUE;
+}
+
+void CryptContextAddRef(void)
+{
+	return TRUE;
+}
+
+void CryptCreateHash(void)
+{
+	return TRUE;
+}
+
+void CryptDecrypt(void)
+{
+	return TRUE;
+}
+
+void CryptDeriveKey(void)
+{
+	return TRUE;
+}
+
+void CryptDestroyHash(void)
+{
+	return TRUE;
+}
+
+void CryptDestroyKey(void)
+{
+	return TRUE;
+}
+
+void CryptDuplicateHash(void)
+{
+	return TRUE;
+}
+
+void CryptDuplicateKey(void)
+{
+	return TRUE;
+}
+
+void CryptEncrypt(void)
+{
+	return TRUE;
+}
+
+void CryptEnumProvidersA(void)
+{
+	return TRUE;
+}
+
+void CryptEnumProvidersW(void)
+{
+	return TRUE;
+}
+
+void CryptEnumProviderTypesA(void)
+{
+	return TRUE;
+}
+
+void CryptEnumProviderTypesW(void)
+{
+	return TRUE;
+}
+
+void CryptExportKey(void)
+{
+	return TRUE;
+}
+
+void CryptGenKey(void)
+{
+	return TRUE;
+}
+
+void CryptGenRandom(void)
+{
+	return TRUE;
+}
+
+void CryptGetDefaultProviderA(void)
+{
+	return TRUE;
+}
+
+void CryptGetDefaultProviderW(void)
+{
+	return TRUE;
+}
+
+void CryptGetHashParam(void)
+{
+	return TRUE;
+}
+
+void CryptGetKeyParam(void)
+{
+	return TRUE;
+}
+
+void CryptGetProvParam(void)
+{
+	return TRUE;
+}
+
+void CryptGetUserKey(void)
+{
+	return TRUE;
+}
+
+void CryptHashData(void)
+{
+	return TRUE;
+}
+
+void CryptHashSessionKey(void)
+{
+	return TRUE;
+}
+
+void CryptImportKey(void)
+{
+	return TRUE;
+}
+
+void CryptReleaseContext(void)
+{
+	return TRUE;
+}
+
+void CryptSetHashParam(void)
+{
+	return TRUE;
+}
+
+void CryptSetKeyParam(void)
+{
+	return TRUE;
+}
+
+void CryptSetProviderA(void)
+{
+	return TRUE;
+}
+
+void CryptSetProviderExA(void)
+{
+	return TRUE;
+}
+
+void CryptSetProviderExW(void)
+{
+	return TRUE;
+}
+
+void CryptSetProviderW(void)
+{
+	return TRUE;
+}
+
+void CryptSetProvParam(void)
+{
+	return TRUE;
+}
+
+void CryptSignHashA(void)
+{
+	return TRUE;
+}
+
+void CryptSignHashW(void)
+{
+	return TRUE;
+}
+
+void CryptVerifySignatureA(void)
+{
+	return TRUE;
+}
+
+void CryptVerifySignatureW(void)
+{
+	return TRUE;
+}
+
