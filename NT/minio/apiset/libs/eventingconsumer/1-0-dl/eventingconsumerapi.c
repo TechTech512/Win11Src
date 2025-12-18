1b7dde9a5deecc5b854bedf670c4659a77d98bf3
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 06:57:26 -0500

    Add files via upload

---
+++ NT/minio/apiset/libs/eventingconsumer/1-0-dl/eventingconsumerapi.c
@@ -0,0 +1,19 @@
+#pragma warning (disable:4098)
+
+#include <windows.h>
+
+void CloseTrace(void)
+{
+	return TRUE;
+}
+
+void OpenTraceW(void)
+{
+	return TRUE;
+}
+
+void ProcessTrace(void)
+{
+	return TRUE;
+}
+
