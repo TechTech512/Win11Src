40cdf8be0e9254f1ae42974311c243f0de5bc1f5
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 08:45:15 -0500

    Add files via upload

---
+++ NT/minio/apiset/libs/lsapolicy/1-0-dl/lsapolicy.c
@@ -0,0 +1,114 @@
+#pragma warning (disable:4098)
+
+#include <windows.h>
+
+void LsaAddAccountRights(void)
+{
+	return TRUE;
+}
+
+void LsaClose(void)
+{
+	return TRUE;
+}
+
+void LsaCreateSecret(void)
+{
+	return TRUE;
+}
+
+void LsaEnumerateAccountRights(void)
+{
+	return TRUE;
+}
+
+void LsaEnumerateAccountsWithUserRight(void)
+{
+	return TRUE;
+}
+
+void LsaFreeMemory(void)
+{
+	return TRUE;
+}
+
+void LsaICLookupNames(void)
+{
+	return TRUE;
+}
+
+void LsaICLookupNamesWithCreds(void)
+{
+	return TRUE;
+}
+
+void LsaICLookupSids(void)
+{
+	return TRUE;
+}
+
+void LsaICLookupSidsWithCreds(void)
+{
+	return TRUE;
+}
+
+void LsaLookupNames2(void)
+{
+	return TRUE;
+}
+
+void LsaLookupSids(void)
+{
+	return TRUE;
+}
+
+void LsaLookupSids2(void)
+{
+	return TRUE;
+}
+
+void LsaOpenPolicy(void)
+{
+	return TRUE;
+}
+
+void LsaOpenSecret(void)
+{
+	return TRUE;
+}
+
+void LsaQueryInformationPolicy(void)
+{
+	return TRUE;
+}
+
+void LsaQuerySecret(void)
+{
+	return TRUE;
+}
+
+void LsaRemoveAccountRights(void)
+{
+	return TRUE;
+}
+
+void LsaRetrievePrivateData(void)
+{
+	return TRUE;
+}
+
+void LsaSetInformationPolicy(void)
+{
+	return TRUE;
+}
+
+void LsaSetSecret(void)
+{
+	return TRUE;
+}
+
+void LsaStorePrivateData(void)
+{
+	return TRUE;
+}
+
