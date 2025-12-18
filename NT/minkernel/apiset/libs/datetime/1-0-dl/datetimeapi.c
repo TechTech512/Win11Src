5f5b5f393488d8f4b4477417a273a4fe78480b0e
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 17:48:52 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/datetime/1-0-dl/datetimeapi.c
@@ -0,0 +1,22 @@
+#pragma warning (disable:4098)
+
+void GetDateFormatA(void)
+{
+	return 1;
+}
+
+void GetDateFormatW(void)
+{
+	return 1;
+}
+
+void GetTimeFormatA(void)
+{
+	return 1;
+}
+
+void GetTimeFormatW(void)
+{
+	return 1;
+}
+
---
+++ NT/minkernel/apiset/libs/datetime/1-1-dl/datetimeapi.c
@@ -0,0 +1,12 @@
+#pragma warning (disable:4098)
+
+void GetDateFormatEx(void)
+{
+	return 1;
+}
+
+void GetTimeFormatEx(void)
+{
+	return 1;
+}
+
