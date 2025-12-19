3fbbf3c47fb9e3b98433b99efdd6a674ad2c021a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-18 19:16:43 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/errorhandling/1-0-dl/errhandlingapi.c
@@ -0,0 +1,37 @@
+#pragma warning (disable:4098)
+
+void GetErrorMode(void)
+{
+	return 1;
+}
+
+void GetLastError(void)
+{
+	return 1;
+}
+
+void RaiseException(void)
+{
+	return 1;
+}
+
+void SetErrorMode(void)
+{
+	return 1;
+}
+
+void SetLastError(void)
+{
+	return 1;
+}
+
+void SetUnhandledExceptionFilter(void)
+{
+	return 1;
+}
+
+void UnhandledExceptionFilter(void)
+{
+	return 1;
+}
+
---
+++ NT/minkernel/apiset/libs/errorhandling/1-1-dl/errhandlingapi.c
@@ -0,0 +1,27 @@
+#pragma warning (disable:4098)
+
+void AddVectoredContinueHandler(void)
+{
+	return 1;
+}
+
+void AddVectoredExceptionHandler(void)
+{
+	return 1;
+}
+
+void RemoveVectoredContinueHandler(void)
+{
+	return 1;
+}
+
+void RemoveVectoredExceptionHandler(void)
+{
+	return 1;
+}
+
+void RestoreLastError(void)
+{
+	return 1;
+}
+
