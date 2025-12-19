d9aad08d30cc4afaff04ab1cf5e382090b0cfcab
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-19 12:04:28 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/handle/1-0-dl/handleapi.c
@@ -0,0 +1,27 @@
+#pragma warning (disable:4098)
+
+void CloseHandle(void)
+{
+	return 1;
+}
+
+void CompareObjectHandles(void)
+{
+	return 1;
+}
+
+void DuplicateHandle(void)
+{
+	return 1;
+}
+
+void GetHandleInformation(void)
+{
+	return 1;
+}
+
+void SetHandleInformation(void)
+{
+	return 1;
+}
+
