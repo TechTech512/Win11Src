6ea6220d1bcc11cbccf6c90ac7e35da3575d76dd
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-19 21:10:09 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/heap/1-0-dl/heapapi.c
@@ -0,0 +1,82 @@
+#pragma warning (disable:4098)
+
+void GetProcessHeap(void)
+{
+	return 1;
+}
+
+void GetProcessHeaps(void)
+{
+	return 1;
+}
+
+void HeapAlloc(void)
+{
+	return 1;
+}
+
+void HeapCompact(void)
+{
+	return 1;
+}
+
+void HeapCreate(void)
+{
+	return 1;
+}
+
+void HeapDestroy(void)
+{
+	return 1;
+}
+
+void HeapFree(void)
+{
+	return 1;
+}
+
+void HeapLock(void)
+{
+	return 1;
+}
+
+void HeapQueryInformation(void)
+{
+	return 1;
+}
+
+void HeapReAlloc(void)
+{
+	return 1;
+}
+
+void HeapSetInformation(void)
+{
+	return 1;
+}
+
+void HeapSize(void)
+{
+	return 1;
+}
+
+void HeapSummary(void)
+{
+	return 1;
+}
+
+void HeapUnlock(void)
+{
+	return 1;
+}
+
+void HeapValidate(void)
+{
+	return 1;
+}
+
+void HeapWalk(void)
+{
+	return 1;
+}
+
