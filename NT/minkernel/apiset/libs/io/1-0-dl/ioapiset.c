422e21ec0d4f5697ff71f33e551d803716f5647a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-20 18:37:56 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/io/1-0-dl/ioapiset.c
@@ -0,0 +1,37 @@
+#pragma warning (disable:4098)
+
+void CancelIoEx(void)
+{
+	return 1;
+}
+
+void CreateIoCompletionPort(void)
+{
+	return 1;
+}
+
+void DeviceIoControl(void)
+{
+	return 1;
+}
+
+void GetOverlappedResult(void)
+{
+	return 1;
+}
+
+void GetQueuedCompletionStatus(void)
+{
+	return 1;
+}
+
+void GetQueuedCompletionStatusEx(void)
+{
+	return 1;
+}
+
+void PostQueuedCompletionStatus(void)
+{
+	return 1;
+}
+
---
+++ NT/minkernel/apiset/libs/io/1-1-dl/ioapiset.c
@@ -0,0 +1,17 @@
+#pragma warning (disable:4098)
+
+void CancelIo(void)
+{
+	return 1;
+}
+
+void CancelSynchronousIo(void)
+{
+	return 1;
+}
+
+void GetOverlappedResultEx(void)
+{
+	return 1;
+}
+
