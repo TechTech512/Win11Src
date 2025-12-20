edbf3449051203b2809a9d5b831a2b1d9c86b546
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-20 09:20:49 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/interlocked/1-0-dl/interlockedapi.c
@@ -0,0 +1,32 @@
+#pragma warning (disable:4098)
+
+void InitializeSListHead(void)
+{
+	return 1;
+}
+
+void InterlockedFlushSList(void)
+{
+	return 1;
+}
+
+void InterlockedPopEntrySList(void)
+{
+	return 1;
+}
+
+void InterlockedPushEntrySList(void)
+{
+	return 1;
+}
+
+void InterlockedPushListSList(void)
+{
+	return 1;
+}
+
+void QueryDepthSList(void)
+{
+	return 1;
+}
+
