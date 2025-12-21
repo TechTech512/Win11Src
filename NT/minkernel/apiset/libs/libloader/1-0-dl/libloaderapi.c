79bfd37aa68f93200abe613b7e77fab27c5b5709
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-20 20:30:05 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/libloader/1-0-dl/libloaderapi.c
@@ -0,0 +1,117 @@
+#pragma warning (disable:4098)
+
+void AddDllDirectory(void)
+{
+	return 1;
+}
+
+void DisableThreadLibraryCalls(void)
+{
+	return 1;
+}
+
+void FindResourceExW(void)
+{
+	return 1;
+}
+
+void FindStringOrdinal(void)
+{
+	return 1;
+}
+
+void FreeLibrary(void)
+{
+	return 1;
+}
+
+void FreeLibraryAndExitThread(void)
+{
+	return 1;
+}
+
+void FreeResource(void)
+{
+	return 1;
+}
+
+void GetModuleFileNameA(void)
+{
+	return 1;
+}
+
+void GetModuleFileNameW(void)
+{
+	return 1;
+}
+
+void GetModuleHandleA(void)
+{
+	return 1;
+}
+
+void GetModuleHandleExA(void)
+{
+	return 1;
+}
+
+void GetModuleHandleExW(void)
+{
+	return 1;
+}
+
+void GetModuleHandleW(void)
+{
+	return 1;
+}
+
+void GetProcAddress(void)
+{
+	return 1;
+}
+
+void LoadLibraryExA(void)
+{
+	return 1;
+}
+
+void LoadLibraryExW(void)
+{
+	return 1;
+}
+
+void LoadResource(void)
+{
+	return 1;
+}
+
+void LoadStringA(void)
+{
+	return 1;
+}
+
+void LoadStringW(void)
+{
+	return 1;
+}
+
+void LockResource(void)
+{
+	return 1;
+}
+
+void RemoveDllDirectory(void)
+{
+	return 1;
+}
+
+void SetDefaultDllDirectories(void)
+{
+	return 1;
+}
+
+void SizeofResource(void)
+{
+	return 1;
+}
+
---
+++ NT/minkernel/apiset/libs/libloader/1-1-dl/libloaderapi.c
@@ -0,0 +1,32 @@
+#pragma warning (disable:4098)
+
+void EnumResourceLanguagesExA(void)
+{
+	return 1;
+}
+
+void EnumResourceLanguagesExW(void)
+{
+	return 1;
+}
+
+void EnumResourceNamesExA(void)
+{
+	return 1;
+}
+
+void EnumResourceNamesExW(void)
+{
+	return 1;
+}
+
+void EnumResourceTypesExA(void)
+{
+	return 1;
+}
+
+void EnumResourceTypesExW(void)
+{
+	return 1;
+}
+
