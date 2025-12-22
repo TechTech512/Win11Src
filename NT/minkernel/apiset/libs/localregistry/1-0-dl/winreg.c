b729e7476bda8ca505832760223ccbe34d8593f4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-21 20:51:02 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/localregistry/1-0-dl/winreg.c
@@ -0,0 +1,217 @@
+#pragma warning (disable:4098)
+
+void RegCloseKey(void)
+{
+	return 1;
+}
+
+void RegCopyTreeW(void)
+{
+	return 1;
+}
+
+void RegCreateKeyExA(void)
+{
+	return 1;
+}
+
+void RegCreateKeyExW(void)
+{
+	return 1;
+}
+
+void RegDeleteKeyExA(void)
+{
+	return 1;
+}
+
+void RegDeleteKeyExW(void)
+{
+	return 1;
+}
+
+void RegDeleteTreeA(void)
+{
+	return 1;
+}
+
+void RegDeleteTreeW(void)
+{
+	return 1;
+}
+
+void RegDeleteValueA(void)
+{
+	return 1;
+}
+
+void RegDeleteValueW(void)
+{
+	return 1;
+}
+
+void RegDisablePredefinedCacheEx(void)
+{
+	return 1;
+}
+
+void RegEnumKeyExA(void)
+{
+	return 1;
+}
+
+void RegEnumKeyExW(void)
+{
+	return 1;
+}
+
+void RegEnumValueA(void)
+{
+	return 1;
+}
+
+void RegEnumValueW(void)
+{
+	return 1;
+}
+
+void RegFlushKey(void)
+{
+	return 1;
+}
+
+void RegGetKeySecurity(void)
+{
+	return 1;
+}
+
+void RegGetValueA(void)
+{
+	return 1;
+}
+
+void RegGetValueW(void)
+{
+	return 1;
+}
+
+void RegLoadAppKeyA(void)
+{
+	return 1;
+}
+
+void RegLoadAppKeyW(void)
+{
+	return 1;
+}
+
+void RegLoadKeyA(void)
+{
+	return 1;
+}
+
+void RegLoadKeyW(void)
+{
+	return 1;
+}
+
+void RegLoadMUIStringA(void)
+{
+	return 1;
+}
+
+void RegLoadMUIStringW(void)
+{
+	return 1;
+}
+
+void RegNotifyChangeKeyValue(void)
+{
+	return 1;
+}
+
+void RegOpenCurrentUser(void)
+{
+	return 1;
+}
+
+void RegOpenKeyExA(void)
+{
+	return 1;
+}
+
+void RegOpenKeyExW(void)
+{
+	return 1;
+}
+
+void RegOpenUserClassesRoot(void)
+{
+	return 1;
+}
+
+void RegQueryInfoKeyA(void)
+{
+	return 1;
+}
+
+void RegQueryInfoKeyW(void)
+{
+	return 1;
+}
+
+void RegQueryValueExA(void)
+{
+	return 1;
+}
+
+void RegQueryValueExW(void)
+{
+	return 1;
+}
+
+void RegRestoreKeyA(void)
+{
+	return 1;
+}
+
+void RegRestoreKeyW(void)
+{
+	return 1;
+}
+
+void RegSaveKeyExA(void)
+{
+	return 1;
+}
+
+void RegSaveKeyExW(void)
+{
+	return 1;
+}
+
+void RegSetKeySecurity(void)
+{
+	return 1;
+}
+
+void RegSetValueExA(void)
+{
+	return 1;
+}
+
+void RegSetValueExW(void)
+{
+	return 1;
+}
+
+void RegUnLoadKeyA(void)
+{
+	return 1;
+}
+
+void RegUnLoadKeyW(void)
+{
+	return 1;
+}
+
