38507d829b6474c0576d6dff3ca537b92c9de619
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-21 19:47:48 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/localizationobsolete/2-0-dl/obsoletelocalization.c
@@ -0,0 +1,52 @@
+#pragma warning (disable:4098)
+
+void CompareStringA(void)
+{
+	return 1;
+}
+
+void EnumLanguageGroupLocalesW(void)
+{
+	return 1;
+}
+
+void EnumSystemLanguageGroupsW(void)
+{
+	return 1;
+}
+
+void EnumUILanguagesW(void)
+{
+	return 1;
+}
+
+void GetNumberFormatW(void)
+{
+	return 1;
+}
+
+void GetStringTypeA(void)
+{
+	return 1;
+}
+
+void GetSystemDefaultLocaleName(void)
+{
+	return 1;
+}
+
+void GetSystemDefaultUILanguage(void)
+{
+	return 1;
+}
+
+void GetUserDefaultUILanguage(void)
+{
+	return 1;
+}
+
+void LCIDToLocaleName(void)
+{
+	return 1;
+}
+
