80bc91083b964d4f08797d0f2dc9d3798b0cc9f4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-23 14:45:42 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/processenv/1-0-dl/processenv.c
@@ -0,0 +1,107 @@
+#pragma warning (disable:4098)
+
+void ExpandEnvironmentStringsA(void)
+{
+	return 1;
+}
+
+void ExpandEnvironmentStringsW(void)
+{
+	return 1;
+}
+
+void FreeEnvironmentStringsA(void)
+{
+	return 1;
+}
+
+void FreeEnvironmentStringsW(void)
+{
+	return 1;
+}
+
+void GetCommandLineA(void)
+{
+	return 1;
+}
+
+void GetCommandLineW(void)
+{
+	return 1;
+}
+
+void GetCurrentDirectoryA(void)
+{
+	return 1;
+}
+
+void GetCurrentDirectoryW(void)
+{
+	return 1;
+}
+
+void GetEnvironmentStrings(void)
+{
+	return 1;
+}
+
+void GetEnvironmentStringsW(void)
+{
+	return 1;
+}
+
+void GetEnvironmentVariableA(void)
+{
+	return 1;
+}
+
+void GetEnvironmentVariableW(void)
+{
+	return 1;
+}
+
+void GetStdHandle(void)
+{
+	return 1;
+}
+
+void SearchPathW(void)
+{
+	return 1;
+}
+
+void SetCurrentDirectoryA(void)
+{
+	return 1;
+}
+
+void SetCurrentDirectoryW(void)
+{
+	return 1;
+}
+
+void SetEnvironmentStringsW(void)
+{
+	return 1;
+}
+
+void SetEnvironmentVariableA(void)
+{
+	return 1;
+}
+
+void SetEnvironmentVariableW(void)
+{
+	return 1;
+}
+
+void SetStdHandle(void)
+{
+	return 1;
+}
+
+void SetStdHandleEx(void)
+{
+	return 1;
+}
+
---
+++ NT/minkernel/apiset/libs/processenv/2-0-dl/processenv.c
@@ -0,0 +1,17 @@
+#pragma warning (disable:4098)
+
+void NeedCurrentDirectoryForExePathA(void)
+{
+	return 1;
+}
+
+void NeedCurrentDirectoryForExePathW(void)
+{
+	return 1;
+}
+
+void SearchPathA(void)
+{
+	return 1;
+}
+
