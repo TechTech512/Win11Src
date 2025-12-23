83e41592aab209f204ec83b03b4f04dedda02e74
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-23 13:36:25 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/namedpipe/1-0-dl/namedpipeapi.c
@@ -0,0 +1,52 @@
+#pragma warning (disable:4098)
+
+void ConnectNamedPipe(void)
+{
+	return 1;
+}
+
+void CreateNamedPipeW(void)
+{
+	return 1;
+}
+
+void CreatePipe(void)
+{
+	return 1;
+}
+
+void DisconnectNamedPipe(void)
+{
+	return 1;
+}
+
+void GetNamedPipeClientComputerNameW(void)
+{
+	return 1;
+}
+
+void ImpersonateNamedPipeClient(void)
+{
+	return 1;
+}
+
+void PeekNamedPipe(void)
+{
+	return 1;
+}
+
+void SetNamedPipeHandleState(void)
+{
+	return 1;
+}
+
+void TransactNamedPipe(void)
+{
+	return 1;
+}
+
+void WaitNamedPipeW(void)
+{
+	return 1;
+}
+
