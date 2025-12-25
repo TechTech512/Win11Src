a19124682d7553051d9d10fa4f74a0a9707d1356
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-25 07:43:23 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/realtime/1-0-dl/realtimeapiset.c
@@ -0,0 +1,27 @@
+#pragma warning (disable:4098)
+
+void QueryIdleProcessorCycleTime(void)
+{
+	return 1;
+}
+
+void QueryIdleProcessorCycleTimeEx(void)
+{
+	return 1;
+}
+
+void QueryProcessCycleTime(void)
+{
+	return 1;
+}
+
+void QueryThreadCycleTime(void)
+{
+	return 1;
+}
+
+void QueryUnbiasedInterruptTime(void)
+{
+	return 1;
+}
+
