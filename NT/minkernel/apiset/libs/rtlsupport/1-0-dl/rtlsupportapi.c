f8ae03d52fb2b7ad887fa507cad32fc94b62194a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-25 08:06:34 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/rtlsupport/1-0-dl/rtlsupportapi.c
@@ -0,0 +1,72 @@
+#pragma warning (disable:4098)
+
+void RtlAddFunctionTable(void)
+{
+	return 1;
+}
+
+void RtlCaptureContext(void)
+{
+	return 1;
+}
+
+void RtlCaptureStackBackTrace(void)
+{
+	return 1;
+}
+
+void RtlCompareMemory(void)
+{
+	return 1;
+}
+
+void RtlDeleteFunctionTable(void)
+{
+	return 1;
+}
+
+void RtlInstallFunctionTableCallback(void)
+{
+	return 1;
+}
+
+void RtlIsEcCode(void)
+{
+	return 1;
+}
+
+void RtlLookupFunctionEntry(void)
+{
+	return 1;
+}
+
+void RtlPcToFileHeader(void)
+{
+	return 1;
+}
+
+void RtlRaiseException(void)
+{
+	return 1;
+}
+
+void RtlRestoreContext(void)
+{
+	return 1;
+}
+
+void RtlUnwind(void)
+{
+	return 1;
+}
+
+void RtlUnwindEx(void)
+{
+	return 1;
+}
+
+void RtlVirtualUnwind(void)
+{
+	return 1;
+}
+
