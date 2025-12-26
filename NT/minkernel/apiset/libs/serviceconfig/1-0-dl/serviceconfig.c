9650711342c7743e37ebc97a9611c790bfe9844b
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-26 05:59:09 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/serviceconfig/1-0-dl/serviceconfig.c
@@ -0,0 +1,8 @@
+void ChangeServiceConfig2W(void){}
+void ChangeServiceConfigW(void){}
+void NotifyServiceStatusChangeW(void){}
+void QueryServiceConfig2W(void){}
+void QueryServiceConfigW(void){}
+void QueryServiceObjectSecurity(void){}
+void QueryServiceStatusEx(void){}
+void SetServiceObjectSecurity(void){}
