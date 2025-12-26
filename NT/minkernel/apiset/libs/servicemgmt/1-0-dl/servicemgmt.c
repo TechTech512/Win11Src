144967fa82e4c4ce9c9e91131b2c9b99ab0db832
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-26 06:57:02 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/servicemgmt/1-0-dl/servicemgmt.c
@@ -0,0 +1,7 @@
+void CloseServiceHandle(void){}
+void ControlServiceExW(void){}
+void CreateServiceW(void){}
+void DeleteService(void){}
+void OpenSCManagerW(void){}
+void OpenServiceW(void){}
+void StartServiceW(void){}
