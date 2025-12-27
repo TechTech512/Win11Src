b1b35926178ab5e881c556118a9a89fea84b6da8
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-27 12:01:27 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/string/1-0-dl/stringapiset.c
@@ -0,0 +1,8 @@
+void CompareStringEx(void){}
+void CompareStringOrdinal(void){}
+void CompareStringW(void){}
+void FoldStringW(void){}
+void GetStringTypeExW(void){}
+void GetStringTypeW(void){}
+void MultiByteToWideChar(void){}
+void WideCharToMultiByte(void){}
