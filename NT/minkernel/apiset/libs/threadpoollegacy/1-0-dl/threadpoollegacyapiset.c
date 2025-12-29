3867555d9f26bb6a42a65ab6b87c947a6f32409a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-29 17:12:39 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/threadpoollegacy/1-0-dl/threadpoollegacyapiset.c
@@ -0,0 +1,8 @@
+void ChangeTimerQueueTimer(void){}
+void CreateTimerQueue(void){}
+void CreateTimerQueueTimer(void){}
+void DeleteTimerQueue(void){}
+void DeleteTimerQueueEx(void){}
+void DeleteTimerQueueTimer(void){}
+void QueueUserWorkItem(void){}
+void UnregisterWaitEx(void){}
