37b11bf24ae5719fec4d91b9117342706b510959
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2025-12-29 20:28:34 -0500

    Add files via upload

---
+++ NT/minkernel/apiset/libs/xstate/1-0-dl/xstateapi.c
@@ -0,0 +1,8 @@
+void RtlCopyExtendedContext(void){}
+void RtlGetEnabledExtendedFeatures(void){}
+void RtlGetExtendedContextLength(void){}
+void RtlGetExtendedFeaturesMask(void){}
+void RtlInitializeExtendedContext(void){}
+void RtlLocateExtendedFeature(void){}
+void RtlLocateLegacyContext(void){}
+void RtlSetExtendedFeaturesMask(void){}
