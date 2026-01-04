d6e05435b96dc407bc133fe16eb4293b6b05634f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-01-04 16:39:11 -0500

    Add files via upload

---
+++ NT/mincore/apiset/libs/lsalookup/1-0-dl/lsalookupapi.c
@@ -0,0 +1,6 @@
+void LookupAccountNameW(void){}
+void LookupAccountSidW(void){}
+void LookupPrivilegeDisplayNameW(void){}
+void LookupPrivilegeNameW(void){}
+void LookupPrivilegeValueW(void){}
+void LsaEnumerateTrustedDomains(void){}
---
+++ NT/mincore/apiset/libs/lsalookup/1-1-dl/lsalookupapi.c
@@ -0,0 +1,1 @@
+void LsaManageSidNameMapping(void){}
