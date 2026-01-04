178c9255fd3fc14ec37658cdb522fef8d7e61213
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-01-04 10:23:20 -0500

    Add files via upload

---
+++ NT/mincore/apiset/libs/eventingclassicprovider/1-0-dl/eventingclassicproviderapi.c
@@ -0,0 +1,8 @@
+void GetTraceEnableFlags(void){}
+void GetTraceEnableLevel(void){}
+void GetTraceLoggerHandle(void){}
+void RegisterTraceGuidsW(void){}
+void TraceEvent(void){}
+void TraceMessage(void){}
+void TraceMessageVa(void){}
+void UnregisterTraceGuids(void){}
