bbe2033115381787f43c17be7711580a3ca0e17f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-01-18 07:57:47 -0500

    Add files via upload

---
+++ NT/onecore/base/ntsetup/winbrand/basebrd/serversolution/resource.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+//
+// Microsoft Windows
+// Copyright (c) 2006 Microsoft Corporation. All rights reserved.
+//
+// Defines for the rc file (basebrd.rc).
+//---------------------------------------------------------------------------
+#define IDS_WINDOWS                                                        10
+#define IDS_WINNT                                                          11
+#define IDS_ULTIMLONG                                                      12
+#define IDS_ULTIMSHOR                                                      13
+#define IDS_LEGALCOPYRIGHT                                                 14
+#define IDS_COMPANYNAME                                                    15
+#define IDS_SRV_ESSEN                                                      16
+#define IDS_GUISRVDESC                                                     17
+#define IDS_WINDOWS_LIVE_ID                                                22
+#define IDS_WINDOWS_LIVE_ID_PLURAL                                         23
+#define IDS_WINDOWS_7                                                      30
+#define IDS_WINDOWS_SERVER_7                                               31
+#define IDS_WINDOWS_8                                                      32
+#define IDS_WINDOWS_SERVER_8                                               33
+#define IDS_WINDOWS_XP_ARM                                                 34
+#define IDS_WINDOWS_81                                                     35
+#define IDS_WINDOWS_SERVER_81                                              36
+#define IDS_WINDOWS_XP_ARM_SE                                              37
+#define IDB_LOGONBANNER1                                                  120
+#define IDB_ABOUT1                                                        121
+#define IDB_LOGONBANNER2                                                 1120
+#define IDB_ABOUT2                                                       1121
+#define IDB_LOGONBANNER3                                                 2120
+#define IDB_ABOUT3                                                       2121
+#define IDRCMETADATA_BRANDING                                    RES_METADATA
