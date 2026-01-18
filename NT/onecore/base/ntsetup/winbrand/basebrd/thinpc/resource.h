298f27e63abe4a9680be22436e15f7b3a2f0255d
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-01-17 19:31:46 -0500

    Add files via upload

---
+++ NT/onecore/base/ntsetup/winbrand/basebrd/thinpc/resource.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+//
+// Microsoft Windows
+// Copyright (c) 2006 Microsoft Corporation. All rights reserved.
+//
+// Defines for the rc file (basebrd.rc).
+//---------------------------------------------------------------------------
+#define IDS_WINDOWS                                                        10
+#define IDS_WINNT                                                          11
+#define IDS_ULTIMLONG                                                      12
+#define IDS_ULTIMSHOR                                                      13
+#define IDS_LEGALCOPYRIGHT                                                 14
+#define IDS_COMPANYNAME                                                    15
+#define IDB_LOGONBANNER1                                                  120
+#define IDB_ABOUT1                                                        121
+#define IDS_WINEMBED                                                      380
+#define IDB_LOGONBANNER2                                                 1120
+#define IDB_ABOUT2                                                       1121
+#define IDB_LOGONBANNER3                                                 2120
+#define IDB_ABOUT3                                                       2121
+#define IDRCMETADATA_BRANDING                                    RES_METADATA
