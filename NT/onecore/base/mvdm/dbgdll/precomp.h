a43036831dfdf5e7c2b89e125062c305be4e69e2
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-02-18 13:25:18 -0500

    Add files via upload

---
+++ NT/onecore/base/mvdm/dbgdll/precomp.h
@@ -0,0 +1,17 @@
+#pragma warning (disable:4996)
+
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <mvdm.h>
+#include <bop.h>
+#include <softpc.h>
+#include <dbgexp.h>
+#include <dbgsvc.h>
+#include <vdmdbg.h>
+#include <dbginfo.h>
+#include <vdm.h>
+#include <nt_vdd.h>
+#include <dbgdll.h>
+#include <winternl.h>
+#include <debugapi.h>
