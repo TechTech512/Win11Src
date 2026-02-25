6d1bd10e0ffc9a52f76c82e1955267e39e7dd558
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-02-25 12:55:32 -0500

    Add files via upload

---
+++ NT/onecore/ds/security/umstartup/s0viewer/wndhook/dllmain.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,
+    DWORD fdwReason,
+    LPVOID lpvReserved
+)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hinstDLL);
+    }
+    
+    return TRUE;
+}
+
