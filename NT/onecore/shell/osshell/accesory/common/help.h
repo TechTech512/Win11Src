a55b18759a7057470e21b9ef8d238414b3164605
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-03-18 19:52:25 -0400

    Add files via upload

---
+++ NT/onecore/shell/osshell/accesory/common/help.h
@@ -0,0 +1,16 @@
+/* help.h
+ *
+ *	definitions for help facility
+ *	most of which comes from FloydR's memo
+ */
+
+
+#define HELP_FIND    0xfffe
+#define HELP_HELP    0xfffc
+#define HELP_FOCUS   0xfffb
+#define HELP_LAST    0xfffa
+#define HELP_KEY2STR 0xfff9
+#define HELP_STR2KEY 0xfff8
+#define HELP_XLATE   0xfff7
+
+BOOL FAR APIENTRY FRequestHelp(HANDLE, HWND, WORD);
