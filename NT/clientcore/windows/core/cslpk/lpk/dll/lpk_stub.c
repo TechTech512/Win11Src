179610d692aa9c453437d723197594eb4cb3e7f4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-06-13 20:07:59 -0400

    Add files via upload

---
+++ NT/clientcore/windows/core/cslpk/lpk/dll/lpk_stub.c
@@ -0,0 +1,14 @@
+#pragma warning (disable:4716)
+
+#include <windows.h>
+
+void LpkDrawTextEx(void){}
+void LpkEditControl(void){}
+void LpkExtTextOut(void){}
+void LpkGetCharacterPlacement(void){}
+void LpkGetTextExtentExPoint(void){}
+void LpkInitialize(void){}
+void LpkPSMTextOut(void){}
+void LpkTabbedTextOut(void){}
+void LpkUseGDIWidthCache(void){}
+void ftsWordBreak(void){}
