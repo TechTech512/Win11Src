c629f10c4885960bab51cac5289b98fe6b086faa
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-06-13 20:33:45 -0400

    Add files via upload

---
+++ NT/onecoreuap/net/wlan/compat/wlanutil/stub.c
@@ -0,0 +1,12 @@
+#pragma warning (disable:4716)
+
+#include <windows.h>
+#include <wlanapi.h>
+
+void WlanIsActiveConsoleUser(void){}
+void WlanSignalValueToBar(void){}
+void WlanSignalValueToBarEx(void){}
+void WlanSsidToDisplayName(void){}
+void WlanStringToSsid(void){}
+void WlanStringToUtf8Ssid(void){}
+void WlanUtf8SsidToDisplayName(void){}
