8128965834b3127f9b96607f18f2c90063076a8f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-07-06 19:05:06 -0400

    Add stub functions for CPU resource management

---
+++ NT/onecoreuap/xbox/gamemode/stubdll/dllmain.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+
+UINT GetExpandedResourceExclusiveCpuCount(void){
+	return E_NOTIMPL;
+}
+
+UINT HasExpandedResources(void){
+	return E_NOTIMPL;
+}
+
+int ReleaseExclusiveCpuSets(void){
+	return E_NOTIMPL;
+}
