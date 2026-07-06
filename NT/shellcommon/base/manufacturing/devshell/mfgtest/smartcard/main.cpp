cfdcf96973441620ac1a8f975210a92ab9de4e01
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-07-05 21:04:21 -0400

    Add files via upload

---
+++ NT/shellcommon/base/manufacturing/devshell/mfgtest/smartcard/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * postest.c
+ *
+ * Simple GPS test utility for Windows.
+ * Displays "GPS Test" to the console and exits.
+ */
+
+#include "precomp.h"
+
+int __cdecl wmain(int argc, wchar_t* argv[])
+{
+    // Disable stdout buffering (write immediately)
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    wprintf(L"Smartcard Reader Test\n\n");
+    return 0;
+}
+
