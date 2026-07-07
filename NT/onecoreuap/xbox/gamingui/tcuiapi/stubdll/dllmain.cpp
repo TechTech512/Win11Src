4c4d5ab516023969e18fd9019f718e788f1c082d
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-07-06 20:36:02 -0400

    Add files via upload

---
+++ NT/onecoreuap/xbox/gamingui/tcuiapi/stubdll/dllmain.cpp
@@ -0,0 +1,105 @@
+#include "pch.h"
+
+int CheckGamingPrivilegeSilently(void){
+	return E_NOTIMPL;
+}
+
+int ShowChangeFriendRelationshipUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowGameInfoUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowProfileCardUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowTitleAchievementsUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int CheckGamingPrivilegeSilentlyForUser(void){
+	return E_NOTIMPL;
+}
+
+int CheckGamingPrivilegeWithUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowGameInviteUI(void){
+	return E_NOTIMPL;
+}
+
+int CheckGamingPrivilegeWithUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowGameInviteUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowGameInviteUIWithContext(void){
+	return E_NOTIMPL;
+}
+
+int ProcessPendingGameUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowChangeFriendRelationshipUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowCustomizeUserProfileUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowFindFriendsUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowGameInfoUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowProfileCardUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowTitleAchievementsUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowUserSettingsUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowCustomizeUserProfileUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowFindFriendsUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowUserSettingsUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowGameInviteUIWithContextForUser(void){
+	return E_NOTIMPL;
+}
+
+int ShowPlayerPickerUI(void){
+	return E_NOTIMPL;
+}
+
+int ShowPlayerPickerUIForUser(void){
+	return E_NOTIMPL;
+}
+
+int TryCancelPendingGameUI(void){
+  return FALSE;
+}
