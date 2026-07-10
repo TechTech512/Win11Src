7a94cae88a28b6d94e00ebba7f9cc9291d9b6cb7
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-07-10 14:14:13 -0400

    Add files via upload

---
+++ NT/onecore/net/tcpip/apis/icmp/icmp.c
@@ -0,0 +1,8 @@
+void IcmpCloseHandle(void){}
+void IcmpCreateFile(void){}
+void IcmpParseReplies(void){}
+void IcmpSendEcho(void){}
+void IcmpSendEcho2(void){}
+void do_echo_rep(void){}
+void do_echo_req(void){}
+void register_icmp(void){}
