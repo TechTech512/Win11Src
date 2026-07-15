62a6e2bc0135245cc3ac86f4693f22df09214a5c
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-07-15 17:29:44 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/userinitext/userinitext.c
@@ -0,0 +1,116 @@
+
+int CreateExplorerSessionKey(void)
+
+{
+                    /* 0x109d  1  CreateExplorerSessionKey
+                       0x109d  4  IsSubDesktopSession
+                       0x109d  5  IsTSAppCompatOn
+                       0x109d  7  PerformXForestLogonCheck
+                       0x109d  11  SetupHotKeyForKeyboardLayout
+                       0x109d  12  UserinitExt */
+  return 0;
+}
+
+void DisplayMessageAndExitWindows(void)
+
+{
+                    /* 0x108c  2  DisplayMessageAndExitWindows
+                       0x108c  6  LoadRemoteFontsAndInitMiscWorker */
+  return;
+}
+
+void* ImmWorker(void)
+
+{
+                    /* 0x108f  3  ImmWorker */
+  return 0;
+}
+
+int IsSubDesktopSession(void)
+
+{
+                    /* 0x109d  1  CreateExplorerSessionKey
+                       0x109d  4  IsSubDesktopSession
+                       0x109d  5  IsTSAppCompatOn
+                       0x109d  7  PerformXForestLogonCheck
+                       0x109d  11  SetupHotKeyForKeyboardLayout
+                       0x109d  12  UserinitExt */
+  return 0;
+}
+
+int IsTSAppCompatOn(void)
+
+{
+                    /* 0x109d  1  CreateExplorerSessionKey
+                       0x109d  4  IsSubDesktopSession
+                       0x109d  5  IsTSAppCompatOn
+                       0x109d  7  PerformXForestLogonCheck
+                       0x109d  11  SetupHotKeyForKeyboardLayout
+                       0x109d  12  UserinitExt */
+  return 0;
+}
+
+void LoadRemoteFontsAndInitMiscWorker(void)
+
+{
+                    /* 0x108c  2  DisplayMessageAndExitWindows
+                       0x108c  6  LoadRemoteFontsAndInitMiscWorker */
+  return;
+}
+
+int PerformXForestLogonCheck(void)
+
+{
+                    /* 0x109d  1  CreateExplorerSessionKey
+                       0x109d  4  IsSubDesktopSession
+                       0x109d  5  IsTSAppCompatOn
+                       0x109d  7  PerformXForestLogonCheck
+                       0x109d  11  SetupHotKeyForKeyboardLayout
+                       0x109d  12  UserinitExt */
+  return 0;
+}
+
+unsigned char ProcesRemoteSessionInitialCommand(void)
+
+{
+                    /* 0x1094  8  ProcesRemoteSessionInitialCommand */
+  return 0;
+}
+
+int ProcessTermSrvIniFiles(void)
+
+{
+                    /* 0x1097  9  ProcessTermSrvIniFiles */
+  return 0;
+}
+
+int SetupHotKeyForKeyboardLayout(void)
+
+{
+                    /* 0x109d  1  CreateExplorerSessionKey
+                       0x109d  4  IsSubDesktopSession
+                       0x109d  5  IsTSAppCompatOn
+                       0x109d  7  PerformXForestLogonCheck
+                       0x109d  11  SetupHotKeyForKeyboardLayout
+                       0x109d  12  UserinitExt */
+  return 0;
+}
+
+void SetShellDesktopSwitchEvent(void)
+
+{
+                    /* 0x109c  10  SetShellDesktopSwitchEvent */
+  return;
+}
+
+int UserinitExt(void)
+
+{
+                    /* 0x109d  1  CreateExplorerSessionKey
+                       0x109d  4  IsSubDesktopSession
+                       0x109d  5  IsTSAppCompatOn
+                       0x109d  7  PerformXForestLogonCheck
+                       0x109d  11  SetupHotKeyForKeyboardLayout
+                       0x109d  12  UserinitExt */
+  return 0;
+}
