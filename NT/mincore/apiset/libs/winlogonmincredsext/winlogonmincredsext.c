f3b683d2fd1dce40ed15aa7e411a2f8630a33d03
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 17:19:17 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/winlogonmincredsext/winlogonmincredsext.c
@@ -0,0 +1,16 @@
+
+int __cdecl MinCredGetLogonCredentials(void)
+
+{
+                    /* 0x108c  1  MinCredGetLogonCredentials
+                       0x108c  2  MinCredGetLogonType */
+  return 0;
+}
+
+int __cdecl MinCredGetLogonType(void)
+
+{
+                    /* 0x108c  1  MinCredGetLogonCredentials
+                       0x108c  2  MinCredGetLogonType */
+  return 0;
+}
