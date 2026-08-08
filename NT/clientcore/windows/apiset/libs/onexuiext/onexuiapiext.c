3200063ba6413271f2c6e1ecec82a0bfd446a363
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 16:56:42 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/onexuiext/onexuiapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl OneXShowUI(void)
+
+{
+                    /* 0x1088  1  OneXShowUI
+                       0x1088  2  OneXShowUIFromEAPCreds */
+  return 0;
+}
+
+int __cdecl OneXShowUIFromEAPCreds(void)
+
+{
+                    /* 0x1088  1  OneXShowUI
+                       0x1088  2  OneXShowUIFromEAPCreds */
+  return 0;
+}
