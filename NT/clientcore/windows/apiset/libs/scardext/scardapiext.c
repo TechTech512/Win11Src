bf5040ec052ed6eaa5a8908a9713f0feb166c05b
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 16:48:27 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/scardext/scardapiext.c
@@ -0,0 +1,160 @@
+
+int __cdecl SCardCancel(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardConnectW(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardDisconnect(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardEstablishContext(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardFreeMemory(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardGetCardTypeProviderNameW(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardGetStatusChangeW(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardListCardsW(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardListReadersW(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
+
+int __cdecl SCardReleaseContext(void)
+
+{
+                    /* 0x1088  1  SCardCancel
+                       0x1088  2  SCardConnectW
+                       0x1088  3  SCardDisconnect
+                       0x1088  4  SCardEstablishContext
+                       0x1088  5  SCardFreeMemory
+                       0x1088  6  SCardGetCardTypeProviderNameW
+                       0x1088  7  SCardGetStatusChangeW
+                       0x1088  8  SCardListCardsW
+                       0x1088  9  SCardListReadersW
+                       0x1088  10  SCardReleaseContext */
+  return 0;
+}
