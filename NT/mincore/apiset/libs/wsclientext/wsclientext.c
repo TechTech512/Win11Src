3c02a0766920ddfd6c09a01f5c71dfb6267669b8
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 15:30:06 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/wsclientext/wsclientext.c
@@ -0,0 +1,72 @@
+
+int __cdecl CheckDeveloperLicense(void)
+
+{
+                    /* 0x1090  1  CheckDeveloperLicense
+                       0x1090  2  GetApplicationURL
+                       0x1090  3  WSEvaluatePackage
+                       0x1090  4  WSGetEvaluatePackageAttempted
+                       0x1090  5  WSLicenseClose
+                       0x1090  6  WSLicenseOpen */
+  return 0;
+}
+
+int __cdecl GetApplicationURL(void)
+
+{
+                    /* 0x1090  1  CheckDeveloperLicense
+                       0x1090  2  GetApplicationURL
+                       0x1090  3  WSEvaluatePackage
+                       0x1090  4  WSGetEvaluatePackageAttempted
+                       0x1090  5  WSLicenseClose
+                       0x1090  6  WSLicenseOpen */
+  return 0;
+}
+
+int __cdecl WSEvaluatePackage(void)
+
+{
+                    /* 0x1090  1  CheckDeveloperLicense
+                       0x1090  2  GetApplicationURL
+                       0x1090  3  WSEvaluatePackage
+                       0x1090  4  WSGetEvaluatePackageAttempted
+                       0x1090  5  WSLicenseClose
+                       0x1090  6  WSLicenseOpen */
+  return 0;
+}
+
+int __cdecl WSGetEvaluatePackageAttempted(void)
+
+{
+                    /* 0x1090  1  CheckDeveloperLicense
+                       0x1090  2  GetApplicationURL
+                       0x1090  3  WSEvaluatePackage
+                       0x1090  4  WSGetEvaluatePackageAttempted
+                       0x1090  5  WSLicenseClose
+                       0x1090  6  WSLicenseOpen */
+  return 0;
+}
+
+int __cdecl WSLicenseClose(void)
+
+{
+                    /* 0x1090  1  CheckDeveloperLicense
+                       0x1090  2  GetApplicationURL
+                       0x1090  3  WSEvaluatePackage
+                       0x1090  4  WSGetEvaluatePackageAttempted
+                       0x1090  5  WSLicenseClose
+                       0x1090  6  WSLicenseOpen */
+  return 0;
+}
+
+int __cdecl WSLicenseOpen(void)
+
+{
+                    /* 0x1090  1  CheckDeveloperLicense
+                       0x1090  2  GetApplicationURL
+                       0x1090  3  WSEvaluatePackage
+                       0x1090  4  WSGetEvaluatePackageAttempted
+                       0x1090  5  WSLicenseClose
+                       0x1090  6  WSLicenseOpen */
+  return 0;
+}
