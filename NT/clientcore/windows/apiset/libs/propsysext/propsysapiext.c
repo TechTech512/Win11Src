2b11c151048075c2425476b95ae14a65d1e9c35c
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 19:54:54 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/propsysext/propsysapiext.c
@@ -0,0 +1,27 @@
+
+int __cdecl PSCreateAdapterFromPropertyStore(void)
+
+{
+                    /* 0x1088  1  PSCreateAdapterFromPropertyStore
+                       0x1088  2  PSCreateMemoryPropertyStore
+                       0x1088  3  PropVariantToBSTR */
+  return 0;
+}
+
+int __cdecl PSCreateMemoryPropertyStore(void)
+
+{
+                    /* 0x1088  1  PSCreateAdapterFromPropertyStore
+                       0x1088  2  PSCreateMemoryPropertyStore
+                       0x1088  3  PropVariantToBSTR */
+  return 0;
+}
+
+int __cdecl PropVariantToBSTR(void)
+
+{
+                    /* 0x1088  1  PSCreateAdapterFromPropertyStore
+                       0x1088  2  PSCreateMemoryPropertyStore
+                       0x1088  3  PropVariantToBSTR */
+  return 0;
+}
