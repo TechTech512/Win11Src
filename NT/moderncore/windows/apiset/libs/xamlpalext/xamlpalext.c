d3a54aabb23f89de48bebcf6c9c5419d16532cf3
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 14:54:00 -0400

    Add files via upload

---
+++ NT/moderncore/windows/apiset/libs/xamlpalext/xamlpalext.c
@@ -0,0 +1,72 @@
+
+int __cdecl CreatePhoneAppBarProxy(void)
+
+{
+                    /* 0x1084  1  CreatePhoneAppBarProxy
+                       0x1084  2  CreateWinTextBoxProxy
+                       0x1084  3  GetThemeServices
+                       0x1084  4  XamlBehaviorEnabled
+                       0x1084  5  XamlPalInitialize
+                       0x1084  6  XamlPalUninitialize */
+  return 0;
+}
+
+int __cdecl CreateWinTextBoxProxy(void)
+
+{
+                    /* 0x1084  1  CreatePhoneAppBarProxy
+                       0x1084  2  CreateWinTextBoxProxy
+                       0x1084  3  GetThemeServices
+                       0x1084  4  XamlBehaviorEnabled
+                       0x1084  5  XamlPalInitialize
+                       0x1084  6  XamlPalUninitialize */
+  return 0;
+}
+
+int __cdecl GetThemeServices(void)
+
+{
+                    /* 0x1084  1  CreatePhoneAppBarProxy
+                       0x1084  2  CreateWinTextBoxProxy
+                       0x1084  3  GetThemeServices
+                       0x1084  4  XamlBehaviorEnabled
+                       0x1084  5  XamlPalInitialize
+                       0x1084  6  XamlPalUninitialize */
+  return 0;
+}
+
+int __cdecl XamlBehaviorEnabled(void)
+
+{
+                    /* 0x1084  1  CreatePhoneAppBarProxy
+                       0x1084  2  CreateWinTextBoxProxy
+                       0x1084  3  GetThemeServices
+                       0x1084  4  XamlBehaviorEnabled
+                       0x1084  5  XamlPalInitialize
+                       0x1084  6  XamlPalUninitialize */
+  return 0;
+}
+
+int __cdecl XamlPalInitialize(void)
+
+{
+                    /* 0x1084  1  CreatePhoneAppBarProxy
+                       0x1084  2  CreateWinTextBoxProxy
+                       0x1084  3  GetThemeServices
+                       0x1084  4  XamlBehaviorEnabled
+                       0x1084  5  XamlPalInitialize
+                       0x1084  6  XamlPalUninitialize */
+  return 0;
+}
+
+int __cdecl XamlPalUninitialize(void)
+
+{
+                    /* 0x1084  1  CreatePhoneAppBarProxy
+                       0x1084  2  CreateWinTextBoxProxy
+                       0x1084  3  GetThemeServices
+                       0x1084  4  XamlBehaviorEnabled
+                       0x1084  5  XamlPalInitialize
+                       0x1084  6  XamlPalUninitialize */
+  return 0;
+}
