aef1bf0bda23537dc4ea4cbe013bdff1824ca38a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 19:09:07 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/spinfext/spinfext.c
@@ -0,0 +1,16 @@
+
+int __cdecl SpInfGetIndirectString(void)
+
+{
+                    /* 0x1084  1  SpInfGetIndirectString
+                       0x1084  2  SpInfIsIndirectString */
+  return 0;
+}
+
+int __cdecl SpInfIsIndirectString(void)
+
+{
+                    /* 0x1084  1  SpInfGetIndirectString
+                       0x1084  2  SpInfIsIndirectString */
+  return 0;
+}
