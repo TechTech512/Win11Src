da398b03f47569c9594dd8795d7f81ae5a38f472
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 18:14:35 -0400

    Add files via upload

---
+++ NT/minio/apiset/libs/crimsonext/crimsonapiext.c
@@ -0,0 +1,91 @@
+
+int __cdecl EvtClose(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
+
+int __cdecl EvtCreateRenderContext(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
+
+int __cdecl EvtExportLog(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
+
+int __cdecl EvtGetChannelConfigProperty(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
+
+int __cdecl EvtOpenChannelConfig(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
+
+int __cdecl EvtRender(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
+
+int __cdecl EvtSubscribe(void)
+
+{
+                    /* 0x108c  1  EvtClose
+                       0x108c  2  EvtCreateRenderContext
+                       0x108c  3  EvtExportLog
+                       0x108c  4  EvtGetChannelConfigProperty
+                       0x108c  5  EvtOpenChannelConfig
+                       0x108c  6  EvtRender
+                       0x108c  7  EvtSubscribe */
+  return 0;
+}
