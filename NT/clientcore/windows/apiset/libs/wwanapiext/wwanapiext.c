28e1ecf5e4a8f528b69abb219f53e9c8fc7a2639
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 15:18:29 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/wwanapiext/wwanapiext.c
@@ -0,0 +1,62 @@
+
+int __cdecl WwanAuthChallenge(void)
+
+{
+                    /* 0x1088  1  WwanAuthChallenge
+                       0x1088  2  WwanCloseHandle
+                       0x1088  4  WwanOpenHandle
+                       0x1088  5  WwanQueryInterface
+                       0x1088  6  WwanRegisterNotification */
+  return 0;
+}
+
+int __cdecl WwanCloseHandle(void)
+
+{
+                    /* 0x1088  1  WwanAuthChallenge
+                       0x1088  2  WwanCloseHandle
+                       0x1088  4  WwanOpenHandle
+                       0x1088  5  WwanQueryInterface
+                       0x1088  6  WwanRegisterNotification */
+  return 0;
+}
+
+void WwanFreeMemory(void)
+
+{
+                    /* 0x108c  3  WwanFreeMemory */
+  return;
+}
+
+int __cdecl WwanOpenHandle(void)
+
+{
+                    /* 0x1088  1  WwanAuthChallenge
+                       0x1088  2  WwanCloseHandle
+                       0x1088  4  WwanOpenHandle
+                       0x1088  5  WwanQueryInterface
+                       0x1088  6  WwanRegisterNotification */
+  return 0;
+}
+
+int __cdecl WwanQueryInterface(void)
+
+{
+                    /* 0x1088  1  WwanAuthChallenge
+                       0x1088  2  WwanCloseHandle
+                       0x1088  4  WwanOpenHandle
+                       0x1088  5  WwanQueryInterface
+                       0x1088  6  WwanRegisterNotification */
+  return 0;
+}
+
+int __cdecl WwanRegisterNotification(void)
+
+{
+                    /* 0x1088  1  WwanAuthChallenge
+                       0x1088  2  WwanCloseHandle
+                       0x1088  4  WwanOpenHandle
+                       0x1088  5  WwanQueryInterface
+                       0x1088  6  WwanRegisterNotification */
+  return 0;
+}
