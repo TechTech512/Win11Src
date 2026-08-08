6ba1f6dbe7f146a977d1be48e31953d3ef91470f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 18:30:09 -0400

    Add files via upload

---
+++ NT/moderncore/windows/apiset/libs/uiacore/uiacoreext.c
@@ -0,0 +1,16 @@
+
+int __cdecl UiaHostProviderFromHwnd(void)
+
+{
+                    /* 0x1084  1  UiaHostProviderFromHwnd
+                       0x1084  2  UiaReturnRawElementProvider */
+  return 0;
+}
+
+int __cdecl UiaReturnRawElementProvider(void)
+
+{
+                    /* 0x1084  1  UiaHostProviderFromHwnd
+                       0x1084  2  UiaReturnRawElementProvider */
+  return 0;
+}
