0ee3d71b1c8259904bbde35e1c9e9a9861431f8a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 18:59:57 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/iscsidscext/iscsidscext.c
@@ -0,0 +1,16 @@
+
+int __cdecl GetIScsiInitiatorNodeNameW(void)
+
+{
+                    /* 0x108c  1  GetIScsiInitiatorNodeNameW
+                       0x108c  2  SetIScsiInitiatorNodeNameW */
+  return 0;
+}
+
+int __cdecl SetIScsiInitiatorNodeNameW(void)
+
+{
+                    /* 0x108c  1  GetIScsiInitiatorNodeNameW
+                       0x108c  2  SetIScsiInitiatorNodeNameW */
+  return 0;
+}
