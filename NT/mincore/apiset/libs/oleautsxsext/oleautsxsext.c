030bee6fb893fa60adad483fac17d10626367453
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 18:46:14 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/oleautsxsext/oleautsxsext.c
@@ -0,0 +1,55 @@
+
+int __cdecl SxsOleAut32MapConfiguredClsidToReferenceClsid(void)
+
+{
+                    /* 0x108c  1  SxsOleAut32MapConfiguredClsidToReferenceClsid
+                       0x108c  2  SxsOleAut32MapIIDToProxyStubCLSID
+                       0x108c  3  SxsOleAut32MapIIDToTLBPath
+                       0x108c  4  SxsOleAut32MapReferenceClsidToConfiguredClsid
+                       0x108c  5  SxsOleAut32RedirectTypeLibrary */
+  return 0;
+}
+
+int __cdecl SxsOleAut32MapIIDToProxyStubCLSID(void)
+
+{
+                    /* 0x108c  1  SxsOleAut32MapConfiguredClsidToReferenceClsid
+                       0x108c  2  SxsOleAut32MapIIDToProxyStubCLSID
+                       0x108c  3  SxsOleAut32MapIIDToTLBPath
+                       0x108c  4  SxsOleAut32MapReferenceClsidToConfiguredClsid
+                       0x108c  5  SxsOleAut32RedirectTypeLibrary */
+  return 0;
+}
+
+int __cdecl SxsOleAut32MapIIDToTLBPath(void)
+
+{
+                    /* 0x108c  1  SxsOleAut32MapConfiguredClsidToReferenceClsid
+                       0x108c  2  SxsOleAut32MapIIDToProxyStubCLSID
+                       0x108c  3  SxsOleAut32MapIIDToTLBPath
+                       0x108c  4  SxsOleAut32MapReferenceClsidToConfiguredClsid
+                       0x108c  5  SxsOleAut32RedirectTypeLibrary */
+  return 0;
+}
+
+int __cdecl SxsOleAut32MapReferenceClsidToConfiguredClsid(void)
+
+{
+                    /* 0x108c  1  SxsOleAut32MapConfiguredClsidToReferenceClsid
+                       0x108c  2  SxsOleAut32MapIIDToProxyStubCLSID
+                       0x108c  3  SxsOleAut32MapIIDToTLBPath
+                       0x108c  4  SxsOleAut32MapReferenceClsidToConfiguredClsid
+                       0x108c  5  SxsOleAut32RedirectTypeLibrary */
+  return 0;
+}
+
+int __cdecl SxsOleAut32RedirectTypeLibrary(void)
+
+{
+                    /* 0x108c  1  SxsOleAut32MapConfiguredClsidToReferenceClsid
+                       0x108c  2  SxsOleAut32MapIIDToProxyStubCLSID
+                       0x108c  3  SxsOleAut32MapIIDToTLBPath
+                       0x108c  4  SxsOleAut32MapReferenceClsidToConfiguredClsid
+                       0x108c  5  SxsOleAut32RedirectTypeLibrary */
+  return 0;
+}
