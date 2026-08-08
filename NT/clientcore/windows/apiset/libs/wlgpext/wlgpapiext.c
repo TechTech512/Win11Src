c272564414f2f980b991c57a263e4432a393f250
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 17:03:52 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/wlgpext/wlgpapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl WLGPADeInit(void)
+
+{
+                    /* 0x108c  1  WLGPADeInit
+                       0x108c  2  WLGPAInit */
+  return 0;
+}
+
+int __cdecl WLGPAInit(void)
+
+{
+                    /* 0x108c  1  WLGPADeInit
+                       0x108c  2  WLGPAInit */
+  return 0;
+}
