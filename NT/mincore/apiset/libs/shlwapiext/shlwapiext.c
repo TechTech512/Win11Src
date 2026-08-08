4ea22f308f98b4e69f8618a067bbc9311a08c7cb
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 19:42:50 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/shlwapiext/shlwapiext.c
@@ -0,0 +1,91 @@
+
+int __cdecl AssocQueryStringW(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
+
+int __cdecl PathIsDirectoryW(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
+
+int __cdecl PathIsNetworkPathW(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
+
+int __cdecl SHCreateWorkerWindowW(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
+
+int __cdecl SHIsChildOrSelf(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
+
+int __cdecl SHPinDllOfCLSID(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
+
+int __cdecl ShellMessageBoxA(void)
+
+{
+                    /* 0x1088  1  AssocQueryStringW
+                       0x1088  2  PathIsDirectoryW
+                       0x1088  3  PathIsNetworkPathW
+                       0x1088  4  SHCreateWorkerWindowW
+                       0x1088  5  SHIsChildOrSelf
+                       0x1088  6  SHPinDllOfCLSID
+                       0x1088  7  ShellMessageBoxA */
+  return 0;
+}
