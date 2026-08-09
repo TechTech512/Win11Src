be99de947e2f5974de185c8b86892904be2b243b
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 12:10:41 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/scesrvext/scesrvext.c
@@ -0,0 +1,16 @@
+
+int __cdecl ScesrvInitializeServer(void)
+
+{
+                    /* 0x1088  1  ScesrvInitializeServer
+                       0x1088  2  ScesrvTerminateServer */
+  return 0;
+}
+
+int __cdecl ScesrvTerminateServer(void)
+
+{
+                    /* 0x1088  1  ScesrvInitializeServer
+                       0x1088  2  ScesrvTerminateServer */
+  return 0;
+}
