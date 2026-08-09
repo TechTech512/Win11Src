38b44e41db9588c855ec235c7b2c7e67f293e21e
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 13:42:49 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/tapi32ext/tapi32apiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl lineInitialize(void)
+
+{
+                    /* 0x1088  1  lineInitialize
+                       0x1088  2  lineTranslateAddress */
+  return 0;
+}
+
+int __cdecl lineTranslateAddress(void)
+
+{
+                    /* 0x1088  1  lineInitialize
+                       0x1088  2  lineTranslateAddress */
+  return 0;
+}
