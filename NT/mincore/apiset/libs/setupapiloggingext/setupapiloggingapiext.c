5e3b8ab0675b002a69039e5646b5e89d7fd66568
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 20:51:10 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/setupapiloggingext/setupapiloggingapiext.c
@@ -0,0 +1,43 @@
+
+void SetupCloseLog(void)
+
+{
+                    /* 0x1090  1  SetupCloseLog
+                       0x1090  5  SetupWriteTextLog */
+  return;
+}
+
+int __cdecl SetupGetThreadLogToken(void)
+
+{
+                    /* 0x108c  2  SetupGetThreadLogToken
+                       0x108c  3  SetupLogErrorW
+                       0x108c  4  SetupOpenLog */
+  return 0;
+}
+
+int __cdecl SetupLogErrorW(void)
+
+{
+                    /* 0x108c  2  SetupGetThreadLogToken
+                       0x108c  3  SetupLogErrorW
+                       0x108c  4  SetupOpenLog */
+  return 0;
+}
+
+int __cdecl SetupOpenLog(void)
+
+{
+                    /* 0x108c  2  SetupGetThreadLogToken
+                       0x108c  3  SetupLogErrorW
+                       0x108c  4  SetupOpenLog */
+  return 0;
+}
+
+void SetupWriteTextLog(void)
+
+{
+                    /* 0x1090  1  SetupCloseLog
+                       0x1090  5  SetupWriteTextLog */
+  return;
+}
