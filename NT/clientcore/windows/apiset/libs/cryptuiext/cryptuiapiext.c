11975c09c71bd74902c2a2f0658ca5e2c9964675
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 10:59:45 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/cryptuiext/cryptuiapiext.c
@@ -0,0 +1,55 @@
+
+int __cdecl CertSelectionGetSerializedBlob(void)
+
+{
+                    /* 0x108c  1  CertSelectionGetSerializedBlob
+                       0x108c  2  CryptUIDlgCertMgr
+                       0x108c  3  CryptUIDlgSelectCertificateA
+                       0x108c  4  CryptUIDlgSelectCertificateFromStore
+                       0x108c  5  CryptUIDlgViewCertificateW */
+  return 0;
+}
+
+int __cdecl CryptUIDlgCertMgr(void)
+
+{
+                    /* 0x108c  1  CertSelectionGetSerializedBlob
+                       0x108c  2  CryptUIDlgCertMgr
+                       0x108c  3  CryptUIDlgSelectCertificateA
+                       0x108c  4  CryptUIDlgSelectCertificateFromStore
+                       0x108c  5  CryptUIDlgViewCertificateW */
+  return 0;
+}
+
+int __cdecl CryptUIDlgSelectCertificateA(void)
+
+{
+                    /* 0x108c  1  CertSelectionGetSerializedBlob
+                       0x108c  2  CryptUIDlgCertMgr
+                       0x108c  3  CryptUIDlgSelectCertificateA
+                       0x108c  4  CryptUIDlgSelectCertificateFromStore
+                       0x108c  5  CryptUIDlgViewCertificateW */
+  return 0;
+}
+
+int __cdecl CryptUIDlgSelectCertificateFromStore(void)
+
+{
+                    /* 0x108c  1  CertSelectionGetSerializedBlob
+                       0x108c  2  CryptUIDlgCertMgr
+                       0x108c  3  CryptUIDlgSelectCertificateA
+                       0x108c  4  CryptUIDlgSelectCertificateFromStore
+                       0x108c  5  CryptUIDlgViewCertificateW */
+  return 0;
+}
+
+int __cdecl CryptUIDlgViewCertificateW(void)
+
+{
+                    /* 0x108c  1  CertSelectionGetSerializedBlob
+                       0x108c  2  CryptUIDlgCertMgr
+                       0x108c  3  CryptUIDlgSelectCertificateA
+                       0x108c  4  CryptUIDlgSelectCertificateFromStore
+                       0x108c  5  CryptUIDlgViewCertificateW */
+  return 0;
+}
