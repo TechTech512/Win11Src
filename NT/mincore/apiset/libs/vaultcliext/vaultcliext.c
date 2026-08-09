f0e4c751f1967e4944c49d7d58680ea649f83536
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 10:41:32 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/vaultcliext/vaultcliext.c
@@ -0,0 +1,98 @@
+
+int __cdecl VaultAddItem(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
+
+int __cdecl VaultCloseVault(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
+
+int __cdecl VaultCreateItemType(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
+
+void VaultFree(void)
+
+{
+                    /* 0x1090  4  VaultFree */
+  return;
+}
+
+int __cdecl VaultGetItem(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
+
+int __cdecl VaultGetItemType(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
+
+int __cdecl VaultOpenVault(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
+
+int __cdecl VaultRemoveItem(void)
+
+{
+                    /* 0x108c  1  VaultAddItem
+                       0x108c  2  VaultCloseVault
+                       0x108c  3  VaultCreateItemType
+                       0x108c  5  VaultGetItem
+                       0x108c  6  VaultGetItemType
+                       0x108c  7  VaultOpenVault
+                       0x108c  8  VaultRemoveItem */
+  return 0;
+}
