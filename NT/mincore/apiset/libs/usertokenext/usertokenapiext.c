a304cb42bda6aa66814964a0326c9eb47e1d17d0
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 10:25:44 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/usertokenext/usertokenapiext.c
@@ -0,0 +1,27 @@
+
+int __cdecl QueryActiveSession(void)
+
+{
+                    /* 0x108c  1  QueryActiveSession
+                       0x108c  2  QueryUserToken
+                       0x108c  3  RegisterUsertokenForNoWinlogon */
+  return 0;
+}
+
+int __cdecl QueryUserToken(void)
+
+{
+                    /* 0x108c  1  QueryActiveSession
+                       0x108c  2  QueryUserToken
+                       0x108c  3  RegisterUsertokenForNoWinlogon */
+  return 0;
+}
+
+int __cdecl RegisterUsertokenForNoWinlogon(void)
+
+{
+                    /* 0x108c  1  QueryActiveSession
+                       0x108c  2  QueryUserToken
+                       0x108c  3  RegisterUsertokenForNoWinlogon */
+  return 0;
+}
