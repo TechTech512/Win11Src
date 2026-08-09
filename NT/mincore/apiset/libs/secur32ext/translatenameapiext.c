89a12a41ea7c576f3ebfc2a2a8a6e7e983c6cee4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 11:54:08 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/secur32ext/translatenameapiext.c
@@ -0,0 +1,34 @@
+
+unsigned short __cdecl GetComputerObjectNameW(void)
+
+{
+                    /* 0x1094  1  GetComputerObjectNameW
+                       0x1094  3  SecpTranslateName
+                       0x1094  4  SecpTranslateNameEx */
+  return 0;
+}
+
+void SecpFreeMemory(void)
+
+{
+                    /* 0x1090  2  SecpFreeMemory */
+  return;
+}
+
+unsigned short __cdecl SecpTranslateName(void)
+
+{
+                    /* 0x1094  1  GetComputerObjectNameW
+                       0x1094  3  SecpTranslateName
+                       0x1094  4  SecpTranslateNameEx */
+  return 0;
+}
+
+unsigned short __cdecl SecpTranslateNameEx(void)
+
+{
+                    /* 0x1094  1  GetComputerObjectNameW
+                       0x1094  3  SecpTranslateName
+                       0x1094  4  SecpTranslateNameEx */
+  return 0;
+}
