cd246da3462b788308b62595ca68ffd17c0d751e
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-08 21:27:05 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/setupapiinfext/setupapiinfext.c
@@ -0,0 +1,47 @@
+
+void SetupCloseInfFile(void)
+
+{
+                    /* 0x1088  1  SetupCloseInfFile */
+  return;
+}
+
+int __cdecl SetupFindFirstLineW(void)
+
+{
+                    /* 0x108c  2  SetupFindFirstLineW
+                       0x108c  3  SetupFindNextLine
+                       0x108c  4  SetupGetLineTextW
+                       0x108c  5  SetupOpenInfFileW */
+  return 0;
+}
+
+int __cdecl SetupFindNextLine(void)
+
+{
+                    /* 0x108c  2  SetupFindFirstLineW
+                       0x108c  3  SetupFindNextLine
+                       0x108c  4  SetupGetLineTextW
+                       0x108c  5  SetupOpenInfFileW */
+  return 0;
+}
+
+int __cdecl SetupGetLineTextW(void)
+
+{
+                    /* 0x108c  2  SetupFindFirstLineW
+                       0x108c  3  SetupFindNextLine
+                       0x108c  4  SetupGetLineTextW
+                       0x108c  5  SetupOpenInfFileW */
+  return 0;
+}
+
+int __cdecl SetupOpenInfFileW(void)
+
+{
+                    /* 0x108c  2  SetupFindFirstLineW
+                       0x108c  3  SetupFindNextLine
+                       0x108c  4  SetupGetLineTextW
+                       0x108c  5  SetupOpenInfFileW */
+  return 0;
+}
