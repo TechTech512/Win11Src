d25d9707887b27ecbbb7010fc13509b490564f71
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 10:10:27 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/wininitext/wininitext.c
@@ -0,0 +1,70 @@
+#pragma warning (disable:4098)
+
+unsigned int __cdecl GetLoggedOnUserCount(void)
+
+{
+                    /* 0x108c  1  GetLoggedOnUserCount
+                       0x108c  2  PrimaryTerminalAndHookWorker
+                       0x108c  3  StartLoadingFontsWorker
+                       0x108c  6  WaitForWinstationShutdown
+                       0x108c  7  WinStationSystemShutdownStartedWorker */
+  return 0;
+}
+
+unsigned long __cdecl PrimaryTerminalAndHookWorker(void)
+
+{
+                    /* 0x108c  1  GetLoggedOnUserCount
+                       0x108c  2  PrimaryTerminalAndHookWorker
+                       0x108c  3  StartLoadingFontsWorker
+                       0x108c  6  WaitForWinstationShutdown
+                       0x108c  7  WinStationSystemShutdownStartedWorker */
+  return 0;
+}
+
+unsigned long __cdecl StartLoadingFontsWorker(void)
+
+{
+                    /* 0x108c  1  GetLoggedOnUserCount
+                       0x108c  2  PrimaryTerminalAndHookWorker
+                       0x108c  3  StartLoadingFontsWorker
+                       0x108c  6  WaitForWinstationShutdown
+                       0x108c  7  WinStationSystemShutdownStartedWorker */
+  return 0;
+}
+
+unsigned short __cdecl UIStartupWorker(void)
+
+{
+                    /* 0x1090  4  UIStartupWorker */
+  return 0;
+}
+
+void UnregisterSession0ViewerWindowHookDll(void)
+
+{
+                    /* 0x1094  5  UnregisterSession0ViewerWindowHookDll */
+  return;
+}
+
+int __cdecl WaitForWinstationShutdown(void)
+
+{
+                    /* 0x108c  1  GetLoggedOnUserCount
+                       0x108c  2  PrimaryTerminalAndHookWorker
+                       0x108c  3  StartLoadingFontsWorker
+                       0x108c  6  WaitForWinstationShutdown
+                       0x108c  7  WinStationSystemShutdownStartedWorker */
+  return 0;
+}
+
+void WinStationSystemShutdownStartedWorker(void)
+
+{
+                    /* 0x108c  1  GetLoggedOnUserCount
+                       0x108c  2  PrimaryTerminalAndHookWorker
+                       0x108c  3  StartLoadingFontsWorker
+                       0x108c  6  WaitForWinstationShutdown
+                       0x108c  7  WinStationSystemShutdownStartedWorker */
+  return 0;
+}
