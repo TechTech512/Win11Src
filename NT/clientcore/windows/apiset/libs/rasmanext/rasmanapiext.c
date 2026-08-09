5d28ff2d8dc97f3c82c036effb5a49d85dbc4ed6
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-09 13:55:44 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/rasmanext/rasmanapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl RasInitialize(void)
+
+{
+                    /* 0x1088  1  RasInitialize
+                       0x1088  2  RasReferenceRasman */
+  return 0;
+}
+
+int __cdecl RasReferenceRasman(void)
+
+{
+                    /* 0x1088  1  RasInitialize
+                       0x1088  2  RasReferenceRasman */
+  return 0;
+}
