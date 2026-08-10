1fd6fe963f2a4f4662779c5e78d864b7c209014d
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 19:19:06 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/chartranslationext/chartranslationapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl CharToOemA(void)
+
+{
+                    /* 0x1094  1  CharToOemA
+                       0x1094  2  CharToOemBuffA */
+  return 0;
+}
+
+int __cdecl CharToOemBuffA(void)
+
+{
+                    /* 0x1094  1  CharToOemA
+                       0x1094  2  CharToOemBuffA */
+  return 0;
+}
