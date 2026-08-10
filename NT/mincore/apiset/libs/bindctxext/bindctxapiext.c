8a17413363f8687483a3a820188871aa160cc4bb
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 16:59:06 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/bindctxext/bindctxapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl CoGetObject(void)
+
+{
+                    /* 0x1088  1  CoGetObject
+                       0x1088  2  CreateBindCtx */
+  return 0;
+}
+
+int __cdecl CreateBindCtx(void)
+
+{
+                    /* 0x1088  1  CoGetObject
+                       0x1088  2  CreateBindCtx */
+  return 0;
+}
