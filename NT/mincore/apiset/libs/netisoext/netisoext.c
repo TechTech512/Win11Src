1ba71e9c26ddee884d63c821bce5a6dce74a6954
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 19:50:48 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/netisoext/netisoext.c
@@ -0,0 +1,16 @@
+
+int __cdecl NetworkIsolationCreateAppContainer(void)
+
+{
+                    /* 0x1088  1  NetworkIsolationCreateAppContainer
+                       0x1088  2  NetworkIsolationDeleteAppContainer */
+  return 0;
+}
+
+int __cdecl NetworkIsolationDeleteAppContainer(void)
+
+{
+                    /* 0x1088  1  NetworkIsolationCreateAppContainer
+                       0x1088  2  NetworkIsolationDeleteAppContainer */
+  return 0;
+}
