231611e1292fe0a7e6b9104d3c5a54a9d5a7740f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 19:29:12 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/caretext/caretext.c
@@ -0,0 +1,112 @@
+
+int __cdecl CreateCaret(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl DestroyCaret(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl GetCaretBlinkTime(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl GetCaretPos(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl HideCaret(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl SetCaretBlinkTime(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl SetCaretPos(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
+
+int __cdecl ShowCaret(void)
+
+{
+                    /* 0x1088  1  CreateCaret
+                       0x1088  2  DestroyCaret
+                       0x1088  3  GetCaretBlinkTime
+                       0x1088  4  GetCaretPos
+                       0x1088  5  HideCaret
+                       0x1088  6  SetCaretBlinkTime
+                       0x1088  7  SetCaretPos
+                       0x1088  8  ShowCaret */
+  return 0;
+}
