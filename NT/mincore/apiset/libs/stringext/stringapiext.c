053da432cd4280ca31b518ef345527831c5f47e0
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 17:30:08 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/stringext/stringapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl CheckDBCSEnabledExt(void)
+
+{
+                    /* 0x1088  1  CheckDBCSEnabledExt
+                       0x1088  2  MBToWCSExt */
+  return 0;
+}
+
+int __cdecl MBToWCSExt(void)
+
+{
+                    /* 0x1088  1  CheckDBCSEnabledExt
+                       0x1088  2  MBToWCSExt */
+  return 0;
+}
