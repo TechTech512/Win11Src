48c817234e2f25c600bd6cb0aa949f9c2c5d42ec
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 18:58:40 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/powermgmtext/powermgmtext.c
@@ -0,0 +1,16 @@
+
+int __cdecl RegisterPowerSettingNotification(void)
+
+{
+                    /* 0x1094  1  RegisterPowerSettingNotification
+                       0x1094  2  UnregisterPowerSettingNotification */
+  return 0;
+}
+
+int __cdecl UnregisterPowerSettingNotification(void)
+
+{
+                    /* 0x1094  1  RegisterPowerSettingNotification
+                       0x1094  2  UnregisterPowerSettingNotification */
+  return 0;
+}
