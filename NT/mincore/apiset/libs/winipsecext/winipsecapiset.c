c12e701fecb155d104c39e6aa4c155045b2d4e6f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 19:43:56 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/winipsecext/winipsecapiset.c
@@ -0,0 +1,23 @@
+
+int __cdecl QueryIKEStatistics(void)
+
+{
+                    /* 0x1090  1  QueryIKEStatistics
+                       0x1090  2  QueryIPSecStatistics */
+  return 0;
+}
+
+int __cdecl QueryIPSecStatistics(void)
+
+{
+                    /* 0x1090  1  QueryIKEStatistics
+                       0x1090  2  QueryIPSecStatistics */
+  return 0;
+}
+
+void SPDApiBufferFree(void)
+
+{
+                    /* 0x1094  3  SPDApiBufferFree */
+  return;
+}
