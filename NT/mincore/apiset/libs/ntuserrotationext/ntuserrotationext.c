b536a382a72f354832feae69907df906218d7dc4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 17:44:07 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/ntuserrotationext/ntuserrotationext.c
@@ -0,0 +1,62 @@
+
+int __cdecl GetDisplayAutoRotationPreferencesByProcessId(void)
+
+{
+                    /* 0x1098  2512  GetDisplayAutoRotationPreferencesByProcessId
+                       0x1098  2513  SetActiveProcess
+                       0x1098  2514  GetAutoRotationState
+                       0x1098  2515  GetDisplayAutoRotationPreferences
+                       0x1098  2516  SetDisplayAutoRotationPreferences */
+  return 0;
+}
+
+int __cdecl SetActiveProcess(void)
+
+{
+                    /* 0x1098  2512  GetDisplayAutoRotationPreferencesByProcessId
+                       0x1098  2513  SetActiveProcess
+                       0x1098  2514  GetAutoRotationState
+                       0x1098  2515  GetDisplayAutoRotationPreferences
+                       0x1098  2516  SetDisplayAutoRotationPreferences */
+  return 0;
+}
+
+int __cdecl GetAutoRotationState(void)
+
+{
+                    /* 0x1098  2512  GetDisplayAutoRotationPreferencesByProcessId
+                       0x1098  2513  SetActiveProcess
+                       0x1098  2514  GetAutoRotationState
+                       0x1098  2515  GetDisplayAutoRotationPreferences
+                       0x1098  2516  SetDisplayAutoRotationPreferences */
+  return 0;
+}
+
+int __cdecl GetDisplayAutoRotationPreferences(void)
+
+{
+                    /* 0x1098  2512  GetDisplayAutoRotationPreferencesByProcessId
+                       0x1098  2513  SetActiveProcess
+                       0x1098  2514  GetAutoRotationState
+                       0x1098  2515  GetDisplayAutoRotationPreferences
+                       0x1098  2516  SetDisplayAutoRotationPreferences */
+  return 0;
+}
+
+int __cdecl SetDisplayAutoRotationPreferences(void)
+
+{
+                    /* 0x1098  2512  GetDisplayAutoRotationPreferencesByProcessId
+                       0x1098  2513  SetActiveProcess
+                       0x1098  2514  GetAutoRotationState
+                       0x1098  2515  GetDisplayAutoRotationPreferences
+                       0x1098  2516  SetDisplayAutoRotationPreferences */
+  return 0;
+}
+
+void DeferredDesktopRotation(void)
+
+{
+                    /* 0x1094  2535  DeferredDesktopRotation */
+  return;
+}
