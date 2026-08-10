44696cc54eadf93ccbb644aa4cb284b890ca026d
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 17:17:13 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/ntusersynch/ntusersynchext.c
@@ -0,0 +1,16 @@
+
+int __cdecl MsgWaitForMultipleObjects(void)
+
+{
+                    /* 0x1088  1  MsgWaitForMultipleObjects
+                       0x1088  2  MsgWaitForMultipleObjectsEx */
+  return 0;
+}
+
+int __cdecl MsgWaitForMultipleObjectsEx(void)
+
+{
+                    /* 0x1088  1  MsgWaitForMultipleObjects
+                       0x1088  2  MsgWaitForMultipleObjectsEx */
+  return 0;
+}
