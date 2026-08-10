6e4e1a7bfaba008fd04dfee3161a07eb20615dc2
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 13:59:16 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/winspoolext/winspoolapiext.c
@@ -0,0 +1,112 @@
+
+int __cdecl ClosePrinter(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl DeletePrinterDataExW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl GetPrinterDataExW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl GetPrinterDriverDirectoryW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl GetPrinterW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl OpenPrinterW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl SetPrinterDataExW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
+
+int __cdecl SetPrinterW(void)
+
+{
+                    /* 0x108c  1  ClosePrinter
+                       0x108c  2  DeletePrinterDataExW
+                       0x108c  3  GetPrinterDataExW
+                       0x108c  4  GetPrinterDriverDirectoryW
+                       0x108c  5  GetPrinterW
+                       0x108c  6  OpenPrinterW
+                       0x108c  7  SetPrinterDataExW
+                       0x108c  8  SetPrinterW */
+  return 0;
+}
