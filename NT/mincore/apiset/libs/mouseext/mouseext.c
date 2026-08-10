f3cc34250256e103333b1dfd24958b95b9d725ad
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 19:12:04 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/mouseext/mouseext.c
@@ -0,0 +1,55 @@
+
+int __cdecl GetCapture(void)
+
+{
+                    /* 0x1088  1  GetCapture
+                       0x1088  2  GetDoubleClickTime
+                       0x1088  3  ReleaseCapture
+                       0x1088  4  SetCapture
+                       0x1088  5  TrackMouseEvent */
+  return 0;
+}
+
+int __cdecl GetDoubleClickTime(void)
+
+{
+                    /* 0x1088  1  GetCapture
+                       0x1088  2  GetDoubleClickTime
+                       0x1088  3  ReleaseCapture
+                       0x1088  4  SetCapture
+                       0x1088  5  TrackMouseEvent */
+  return 0;
+}
+
+int __cdecl ReleaseCapture(void)
+
+{
+                    /* 0x1088  1  GetCapture
+                       0x1088  2  GetDoubleClickTime
+                       0x1088  3  ReleaseCapture
+                       0x1088  4  SetCapture
+                       0x1088  5  TrackMouseEvent */
+  return 0;
+}
+
+int __cdecl SetCapture(void)
+
+{
+                    /* 0x1088  1  GetCapture
+                       0x1088  2  GetDoubleClickTime
+                       0x1088  3  ReleaseCapture
+                       0x1088  4  SetCapture
+                       0x1088  5  TrackMouseEvent */
+  return 0;
+}
+
+int __cdecl TrackMouseEvent(void)
+
+{
+                    /* 0x1088  1  GetCapture
+                       0x1088  2  GetDoubleClickTime
+                       0x1088  3  ReleaseCapture
+                       0x1088  4  SetCapture
+                       0x1088  5  TrackMouseEvent */
+  return 0;
+}
