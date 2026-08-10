1aa51fb746e478887a8a2b9b8fabb894f2233ec9
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 17:54:49 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/rectext/rectapiext.c
@@ -0,0 +1,187 @@
+
+int __cdecl CopyRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl EqualRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl InflateRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl IntersectRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl IsRectEmpty(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl OffsetRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl PtInRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl SetRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl SetRectEmpty(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl SubtractRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
+
+int __cdecl UnionRect(void)
+
+{
+                    /* 0x1090  1  CopyRect
+                       0x1090  2  EqualRect
+                       0x1090  3  InflateRect
+                       0x1090  4  IntersectRect
+                       0x1090  5  IsRectEmpty
+                       0x1090  6  OffsetRect
+                       0x1090  7  PtInRect
+                       0x1090  8  SetRect
+                       0x1090  9  SetRectEmpty
+                       0x1090  10  SubtractRect
+                       0x1090  11  UnionRect */
+  return 0;
+}
