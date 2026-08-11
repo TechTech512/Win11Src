f235a42bec9080610d6c627e9e748e59fdb4a4a0
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 10:12:26 -0400

    Add files via upload

---
+++ NT/minkernel/apiset/libs/errorhandlingext/errorhandlingapiext.c
@@ -0,0 +1,40 @@
+
+int __cdecl BasepReportFault(void)
+
+{
+                    /* 0x1094  1  BasepReportFault
+                       0x1094  2  CheckForReadOnlyResourceFilter
+                       0x1094  3  WerpGetDebugger
+                       0x1094  4  WerpLaunchAeDebug */
+  return 0;
+}
+
+int __cdecl CheckForReadOnlyResourceFilter(void)
+
+{
+                    /* 0x1094  1  BasepReportFault
+                       0x1094  2  CheckForReadOnlyResourceFilter
+                       0x1094  3  WerpGetDebugger
+                       0x1094  4  WerpLaunchAeDebug */
+  return 0;
+}
+
+int __cdecl WerpGetDebugger(void)
+
+{
+                    /* 0x1094  1  BasepReportFault
+                       0x1094  2  CheckForReadOnlyResourceFilter
+                       0x1094  3  WerpGetDebugger
+                       0x1094  4  WerpLaunchAeDebug */
+  return 0;
+}
+
+int __cdecl WerpLaunchAeDebug(void)
+
+{
+                    /* 0x1094  1  BasepReportFault
+                       0x1094  2  CheckForReadOnlyResourceFilter
+                       0x1094  3  WerpGetDebugger
+                       0x1094  4  WerpLaunchAeDebug */
+  return 0;
+}
