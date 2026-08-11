c04621b2e32de751476be438988dd82a751a76e4
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 18:39:44 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/comclbcatqext/clbcatqapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl GetCatalogObject(void)
+
+{
+                    /* 0x1088  1  GetCatalogObject
+                       0x1088  2  GetCatalogObject2 */
+  return 0;
+}
+
+int __cdecl GetCatalogObject2(void)
+
+{
+                    /* 0x1088  1  GetCatalogObject
+                       0x1088  2  GetCatalogObject2 */
+  return 0;
+}
