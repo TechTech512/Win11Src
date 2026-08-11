182157f010ab6b104fce1f098e2131a92cc0b1b8
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 13:27:44 -0400

    Add files via upload

---
+++ NT/moderncore/windows/apiset/libs/globalization/collation/globalization-collation.c
@@ -0,0 +1,27 @@
+
+int __cdecl WGCGetCharacterGroupDisplayName(void)
+
+{
+                    /* 0x1094  1  WGCGetCharacterGroupDisplayName
+                       0x1094  2  WGCGetDefaultGroupingLetters
+                       0x1094  3  WGCGetGroupingLetter */
+  return 0;
+}
+
+int __cdecl WGCGetDefaultGroupingLetters(void)
+
+{
+                    /* 0x1094  1  WGCGetCharacterGroupDisplayName
+                       0x1094  2  WGCGetDefaultGroupingLetters
+                       0x1094  3  WGCGetGroupingLetter */
+  return 0;
+}
+
+int __cdecl WGCGetGroupingLetter(void)
+
+{
+                    /* 0x1094  1  WGCGetCharacterGroupDisplayName
+                       0x1094  2  WGCGetDefaultGroupingLetters
+                       0x1094  3  WGCGetGroupingLetter */
+  return 0;
+}
