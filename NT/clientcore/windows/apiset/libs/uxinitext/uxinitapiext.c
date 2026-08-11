7cb8b788cfbc9b76781c0dc63f04f399affb5c76
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 10:55:21 -0400

    Add files via upload

---
+++ NT/clientcore/windows/apiset/libs/uxinitext/uxinitapiext.c
@@ -0,0 +1,91 @@
+
+void ThemesOnCreateSession(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
+
+void ThemesOnDisconnect(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
+
+void ThemesOnEarlyCreateSession(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
+
+void ThemesOnLogoff(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
+
+void ThemesOnLogon(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
+
+void ThemesOnReconnect(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
+
+void ThemesOnTerminateSession(void)
+
+{
+                    /* 0x1088  1  ThemesOnCreateSession
+                       0x1088  2  ThemesOnDisconnect
+                       0x1088  3  ThemesOnEarlyCreateSession
+                       0x1088  4  ThemesOnLogoff
+                       0x1088  5  ThemesOnLogon
+                       0x1088  6  ThemesOnReconnect
+                       0x1088  7  ThemesOnTerminateSession */
+  return;
+}
