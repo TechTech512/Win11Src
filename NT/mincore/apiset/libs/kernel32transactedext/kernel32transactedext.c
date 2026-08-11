8c73a1c240c1d5fcc647a83af428aac2986ea793
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 20:39:12 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/kernel32transactedext/kernel32transactedext.c
@@ -0,0 +1,55 @@
+
+int __cdecl CreateDirectoryTransactedW(void)
+
+{
+                    /* 0x1090  1  CreateDirectoryTransactedW
+                       0x1090  2  DeleteFileTransactedW
+                       0x1090  3  FindFirstFileTransactedW
+                       0x1090  4  MoveFileTransactedW
+                       0x1090  5  RemoveDirectoryTransactedW */
+  return 0;
+}
+
+int __cdecl DeleteFileTransactedW(void)
+
+{
+                    /* 0x1090  1  CreateDirectoryTransactedW
+                       0x1090  2  DeleteFileTransactedW
+                       0x1090  3  FindFirstFileTransactedW
+                       0x1090  4  MoveFileTransactedW
+                       0x1090  5  RemoveDirectoryTransactedW */
+  return 0;
+}
+
+int __cdecl FindFirstFileTransactedW(void)
+
+{
+                    /* 0x1090  1  CreateDirectoryTransactedW
+                       0x1090  2  DeleteFileTransactedW
+                       0x1090  3  FindFirstFileTransactedW
+                       0x1090  4  MoveFileTransactedW
+                       0x1090  5  RemoveDirectoryTransactedW */
+  return 0;
+}
+
+int __cdecl MoveFileTransactedW(void)
+
+{
+                    /* 0x1090  1  CreateDirectoryTransactedW
+                       0x1090  2  DeleteFileTransactedW
+                       0x1090  3  FindFirstFileTransactedW
+                       0x1090  4  MoveFileTransactedW
+                       0x1090  5  RemoveDirectoryTransactedW */
+  return 0;
+}
+
+int __cdecl RemoveDirectoryTransactedW(void)
+
+{
+                    /* 0x1090  1  CreateDirectoryTransactedW
+                       0x1090  2  DeleteFileTransactedW
+                       0x1090  3  FindFirstFileTransactedW
+                       0x1090  4  MoveFileTransactedW
+                       0x1090  5  RemoveDirectoryTransactedW */
+  return 0;
+}
