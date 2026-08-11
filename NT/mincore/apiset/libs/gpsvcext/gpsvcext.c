13706cc8cc95cb5ec796e33ceb1debd6b535d23e
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 11:16:54 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/gpsvcext/gpsvcext.c
@@ -0,0 +1,72 @@
+
+int __cdecl ProcessGroupPolicyCompletedExInternal(void)
+
+{
+                    /* 0x108c  1  ProcessGroupPolicyCompletedExInternal
+                       0x108c  2  ProcessGroupPolicyCompletedInternal
+                       0x108c  3  RsopAccessCheckByTypeInternal
+                       0x108c  4  RsopFileAccessCheckInternal
+                       0x108c  5  RsopResetPolicySettingStatusInternal
+                       0x108c  6  RsopSetPolicySettingStatusInternal */
+  return 0;
+}
+
+int __cdecl ProcessGroupPolicyCompletedInternal(void)
+
+{
+                    /* 0x108c  1  ProcessGroupPolicyCompletedExInternal
+                       0x108c  2  ProcessGroupPolicyCompletedInternal
+                       0x108c  3  RsopAccessCheckByTypeInternal
+                       0x108c  4  RsopFileAccessCheckInternal
+                       0x108c  5  RsopResetPolicySettingStatusInternal
+                       0x108c  6  RsopSetPolicySettingStatusInternal */
+  return 0;
+}
+
+int __cdecl RsopAccessCheckByTypeInternal(void)
+
+{
+                    /* 0x108c  1  ProcessGroupPolicyCompletedExInternal
+                       0x108c  2  ProcessGroupPolicyCompletedInternal
+                       0x108c  3  RsopAccessCheckByTypeInternal
+                       0x108c  4  RsopFileAccessCheckInternal
+                       0x108c  5  RsopResetPolicySettingStatusInternal
+                       0x108c  6  RsopSetPolicySettingStatusInternal */
+  return 0;
+}
+
+int __cdecl RsopFileAccessCheckInternal(void)
+
+{
+                    /* 0x108c  1  ProcessGroupPolicyCompletedExInternal
+                       0x108c  2  ProcessGroupPolicyCompletedInternal
+                       0x108c  3  RsopAccessCheckByTypeInternal
+                       0x108c  4  RsopFileAccessCheckInternal
+                       0x108c  5  RsopResetPolicySettingStatusInternal
+                       0x108c  6  RsopSetPolicySettingStatusInternal */
+  return 0;
+}
+
+int __cdecl RsopResetPolicySettingStatusInternal(void)
+
+{
+                    /* 0x108c  1  ProcessGroupPolicyCompletedExInternal
+                       0x108c  2  ProcessGroupPolicyCompletedInternal
+                       0x108c  3  RsopAccessCheckByTypeInternal
+                       0x108c  4  RsopFileAccessCheckInternal
+                       0x108c  5  RsopResetPolicySettingStatusInternal
+                       0x108c  6  RsopSetPolicySettingStatusInternal */
+  return 0;
+}
+
+int __cdecl RsopSetPolicySettingStatusInternal(void)
+
+{
+                    /* 0x108c  1  ProcessGroupPolicyCompletedExInternal
+                       0x108c  2  ProcessGroupPolicyCompletedInternal
+                       0x108c  3  RsopAccessCheckByTypeInternal
+                       0x108c  4  RsopFileAccessCheckInternal
+                       0x108c  5  RsopResetPolicySettingStatusInternal
+                       0x108c  6  RsopSetPolicySettingStatusInternal */
+  return 0;
+}
