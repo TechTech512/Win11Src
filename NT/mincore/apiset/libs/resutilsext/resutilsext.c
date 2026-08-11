851c002a973a0f814ff200c4e2cdcd17625518f8
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 18:56:13 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/resutilsext/resutilsext.c
@@ -0,0 +1,16 @@
+
+int __cdecl ClusterGetVolumePathName(void)
+
+{
+                    /* 0x108c  1  ClusterGetVolumePathName
+                       0x108c  2  ResUtilGetCoreClusterResources */
+  return 0;
+}
+
+int __cdecl ResUtilGetCoreClusterResources(void)
+
+{
+                    /* 0x108c  1  ClusterGetVolumePathName
+                       0x108c  2  ResUtilGetCoreClusterResources */
+  return 0;
+}
