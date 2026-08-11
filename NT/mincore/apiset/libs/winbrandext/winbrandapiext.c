5e79be08d87f90c1e65d7469906c9894bee03a17
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 19:21:40 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/winbrandext/winbrandapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl BrandingFormatString(void)
+
+{
+                    /* 0x108c  1  BrandingFormatString
+                       0x108c  2  BrandingLoadString */
+  return 0;
+}
+
+int __cdecl BrandingLoadString(void)
+
+{
+                    /* 0x108c  1  BrandingFormatString
+                       0x108c  2  BrandingLoadString */
+  return 0;
+}
