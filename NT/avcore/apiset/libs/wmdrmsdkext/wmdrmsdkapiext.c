435da21b0d148a219f95f9b91a4f5c534cf7273a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 20:23:48 -0400

    Add files via upload

---
+++ NT/avcore/apiset/libs/wmdrmsdkext/wmdrmsdkapiext.c
@@ -0,0 +1,27 @@
+
+int __cdecl MFWMDRMOutputTrustAuthority_CreateInstance(void)
+
+{
+                    /* 0x1088  1  MFWMDRMOutputTrustAuthority_CreateInstance
+                       0x1088  2  WMDRMCreateProvider
+                       0x1088  3  _WMDRMCreateProvider */
+  return 0;
+}
+
+int __cdecl WMDRMCreateProvider(void)
+
+{
+                    /* 0x1088  1  MFWMDRMOutputTrustAuthority_CreateInstance
+                       0x1088  2  WMDRMCreateProvider
+                       0x1088  3  _WMDRMCreateProvider */
+  return 0;
+}
+
+int __cdecl _WMDRMCreateProvider(void)
+
+{
+                    /* 0x1088  1  MFWMDRMOutputTrustAuthority_CreateInstance
+                       0x1088  2  WMDRMCreateProvider
+                       0x1088  3  _WMDRMCreateProvider */
+  return 0;
+}
