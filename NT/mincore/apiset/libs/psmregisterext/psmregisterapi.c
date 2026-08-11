1df9ec6c975997b09e117e2c3d00c63f6c8ad1b0
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 18:27:54 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/psmregisterext/psmregisterapi.c
@@ -0,0 +1,55 @@
+
+int __cdecl PsmActivateApplicationByToken(void)
+
+{
+                    /* 0x108c  1  PsmActivateApplicationByToken
+                       0x108c  2  PsmAdjustActivationToken
+                       0x108c  3  PsmCreateMatchToken
+                       0x108c  4  PsmQueryBackgroundActivationType
+                       0x108c  5  PsmRegisterApplicationProcess */
+  return 0;
+}
+
+int __cdecl PsmAdjustActivationToken(void)
+
+{
+                    /* 0x108c  1  PsmActivateApplicationByToken
+                       0x108c  2  PsmAdjustActivationToken
+                       0x108c  3  PsmCreateMatchToken
+                       0x108c  4  PsmQueryBackgroundActivationType
+                       0x108c  5  PsmRegisterApplicationProcess */
+  return 0;
+}
+
+int __cdecl PsmCreateMatchToken(void)
+
+{
+                    /* 0x108c  1  PsmActivateApplicationByToken
+                       0x108c  2  PsmAdjustActivationToken
+                       0x108c  3  PsmCreateMatchToken
+                       0x108c  4  PsmQueryBackgroundActivationType
+                       0x108c  5  PsmRegisterApplicationProcess */
+  return 0;
+}
+
+int __cdecl PsmQueryBackgroundActivationType(void)
+
+{
+                    /* 0x108c  1  PsmActivateApplicationByToken
+                       0x108c  2  PsmAdjustActivationToken
+                       0x108c  3  PsmCreateMatchToken
+                       0x108c  4  PsmQueryBackgroundActivationType
+                       0x108c  5  PsmRegisterApplicationProcess */
+  return 0;
+}
+
+int __cdecl PsmRegisterApplicationProcess(void)
+
+{
+                    /* 0x108c  1  PsmActivateApplicationByToken
+                       0x108c  2  PsmAdjustActivationToken
+                       0x108c  3  PsmCreateMatchToken
+                       0x108c  4  PsmQueryBackgroundActivationType
+                       0x108c  5  PsmRegisterApplicationProcess */
+  return 0;
+}
