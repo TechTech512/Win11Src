1634bcd8dabf216bacd85ad055b9065cb7f512e6
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 20:53:03 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/quirksext/quirksapiext.c
@@ -0,0 +1,112 @@
+
+int __cdecl QuirkGetData2Worker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkGetDataWorker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkIsEnabled2Worker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkIsEnabled3Worker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkIsEnabledForPackage2Worker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkIsEnabledForPackageWorker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkIsEnabledForProcessWorker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
+
+int __cdecl QuirkIsEnabledWorker(void)
+
+{
+                    /* 0x108c  1  QuirkGetData2Worker
+                       0x108c  2  QuirkGetDataWorker
+                       0x108c  3  QuirkIsEnabled2Worker
+                       0x108c  4  QuirkIsEnabled3Worker
+                       0x108c  5  QuirkIsEnabledForPackage2Worker
+                       0x108c  6  QuirkIsEnabledForPackageWorker
+                       0x108c  7  QuirkIsEnabledForProcessWorker
+                       0x108c  8  QuirkIsEnabledWorker */
+  return 0;
+}
