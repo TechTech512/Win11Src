b0ca58b646cbee562463b8380ab72d55af60c96f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 16:27:27 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/dccreateext/dccreateext.c
@@ -0,0 +1,40 @@
+
+int __cdecl CreateCompatibleDC(void)
+
+{
+                    /* 0x1088  1  CreateCompatibleDC
+                       0x1088  2  CreateDCW
+                       0x1088  3  CreateICW
+                       0x1088  4  DeleteDC */
+  return 0;
+}
+
+int __cdecl CreateDCW(void)
+
+{
+                    /* 0x1088  1  CreateCompatibleDC
+                       0x1088  2  CreateDCW
+                       0x1088  3  CreateICW
+                       0x1088  4  DeleteDC */
+  return 0;
+}
+
+int __cdecl CreateICW(void)
+
+{
+                    /* 0x1088  1  CreateCompatibleDC
+                       0x1088  2  CreateDCW
+                       0x1088  3  CreateICW
+                       0x1088  4  DeleteDC */
+  return 0;
+}
+
+int __cdecl DeleteDC(void)
+
+{
+                    /* 0x1088  1  CreateCompatibleDC
+                       0x1088  2  CreateDCW
+                       0x1088  3  CreateICW
+                       0x1088  4  DeleteDC */
+  return 0;
+}
