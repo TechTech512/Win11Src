85324e7ee56052ecd3255cf05f07710bb6753587
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 10:02:31 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/fileext/fileapiext.c
@@ -0,0 +1,91 @@
+
+int __cdecl BasepCopyEncryption(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
+
+int __cdecl BasepGetComputerNameFromNtPath(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
+
+int __cdecl BasepSetFileEncryptionCompression(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
+
+int __cdecl DuplicateEncryptionInfoFileExt(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
+
+int __cdecl GetEncryptedFileVersionExt(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
+
+int __cdecl OpenConsoleWStub(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
+
+int __cdecl SetVolumeMountPointWStub(void)
+
+{
+                    /* 0x1088  1  BasepCopyEncryption
+                       0x1088  2  BasepGetComputerNameFromNtPath
+                       0x1088  3  BasepSetFileEncryptionCompression
+                       0x1088  4  DuplicateEncryptionInfoFileExt
+                       0x1088  5  GetEncryptedFileVersionExt
+                       0x1088  6  OpenConsoleWStub
+                       0x1088  7  SetVolumeMountPointWStub */
+  return 0;
+}
