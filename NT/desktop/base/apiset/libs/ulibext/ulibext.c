4ff8b412100ac291e02c8941d5760716d6a0b689
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 17:08:57 -0400

    Add files via upload

---
+++ NT/desktop/base/apiset/libs/ulibext/ulibext.c
@@ -0,0 +1,55 @@
+
+int __cdecl FindFirstVolumeMountPointWStub(void)
+
+{
+                    /* 0x108c  1  FindFirstVolumeMountPointWStub
+                       0x108c  2  FindNextVolumeMountPointWStub
+                       0x108c  3  FindVolumeMountPointCloseStub
+                       0x108c  4  SetThreadUILanguageStub
+                       0x108c  5  SystemParametersInfoWStub */
+  return 0;
+}
+
+int __cdecl FindNextVolumeMountPointWStub(void)
+
+{
+                    /* 0x108c  1  FindFirstVolumeMountPointWStub
+                       0x108c  2  FindNextVolumeMountPointWStub
+                       0x108c  3  FindVolumeMountPointCloseStub
+                       0x108c  4  SetThreadUILanguageStub
+                       0x108c  5  SystemParametersInfoWStub */
+  return 0;
+}
+
+int __cdecl FindVolumeMountPointCloseStub(void)
+
+{
+                    /* 0x108c  1  FindFirstVolumeMountPointWStub
+                       0x108c  2  FindNextVolumeMountPointWStub
+                       0x108c  3  FindVolumeMountPointCloseStub
+                       0x108c  4  SetThreadUILanguageStub
+                       0x108c  5  SystemParametersInfoWStub */
+  return 0;
+}
+
+int __cdecl SetThreadUILanguageStub(void)
+
+{
+                    /* 0x108c  1  FindFirstVolumeMountPointWStub
+                       0x108c  2  FindNextVolumeMountPointWStub
+                       0x108c  3  FindVolumeMountPointCloseStub
+                       0x108c  4  SetThreadUILanguageStub
+                       0x108c  5  SystemParametersInfoWStub */
+  return 0;
+}
+
+int __cdecl SystemParametersInfoWStub(void)
+
+{
+                    /* 0x108c  1  FindFirstVolumeMountPointWStub
+                       0x108c  2  FindNextVolumeMountPointWStub
+                       0x108c  3  FindVolumeMountPointCloseStub
+                       0x108c  4  SetThreadUILanguageStub
+                       0x108c  5  SystemParametersInfoWStub */
+  return 0;
+}
