a5d739b4ef5092dccba962a34fb70e758a87c03c
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 21:05:24 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/kernel32packagecurrentext/kernel32packagecurrentext.c
@@ -0,0 +1,16 @@
+
+int __cdecl GetCurrentPackageId(void)
+
+{
+                    /* 0x1094  1  GetCurrentPackageId
+                       0x1094  2  GetCurrentPackageInfo */
+  return 0;
+}
+
+int __cdecl GetCurrentPackageInfo(void)
+
+{
+                    /* 0x1094  1  GetCurrentPackageId
+                       0x1094  2  GetCurrentPackageInfo */
+  return 0;
+}
