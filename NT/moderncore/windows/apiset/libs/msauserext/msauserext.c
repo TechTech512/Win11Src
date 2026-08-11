ee25579e64db0614dc57d50c7ec06f934250c1ac
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 20:08:10 -0400

    Add files via upload

---
+++ NT/moderncore/windows/apiset/libs/msauserext/msauserext.c
@@ -0,0 +1,40 @@
+
+int __cdecl MsaUser_FormatUserDisplayName(void)
+
+{
+                    /* 0x1084  1  MsaUser_FormatUserDisplayName
+                       0x1084  2  MsaUser_GetPlatformQualifier
+                       0x1084  3  MsaUser_IsChildAccount
+                       0x1084  4  MsaUser_WinBioSetMSACredential */
+  return 0;
+}
+
+int __cdecl MsaUser_GetPlatformQualifier(void)
+
+{
+                    /* 0x1084  1  MsaUser_FormatUserDisplayName
+                       0x1084  2  MsaUser_GetPlatformQualifier
+                       0x1084  3  MsaUser_IsChildAccount
+                       0x1084  4  MsaUser_WinBioSetMSACredential */
+  return 0;
+}
+
+int __cdecl MsaUser_IsChildAccount(void)
+
+{
+                    /* 0x1084  1  MsaUser_FormatUserDisplayName
+                       0x1084  2  MsaUser_GetPlatformQualifier
+                       0x1084  3  MsaUser_IsChildAccount
+                       0x1084  4  MsaUser_WinBioSetMSACredential */
+  return 0;
+}
+
+int __cdecl MsaUser_WinBioSetMSACredential(void)
+
+{
+                    /* 0x1084  1  MsaUser_FormatUserDisplayName
+                       0x1084  2  MsaUser_GetPlatformQualifier
+                       0x1084  3  MsaUser_IsChildAccount
+                       0x1084  4  MsaUser_WinBioSetMSACredential */
+  return 0;
+}
