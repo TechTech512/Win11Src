b0a65aa31deef00adafd34c8fe0419972b2fac9e
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 18:03:26 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/psmserviceext/psmserviceext.c
@@ -0,0 +1,27 @@
+
+void PsmApplicationStateNotification(void)
+
+{
+                    /* 0x108c  1  PsmApplicationStateNotification
+                       0x108c  2  PsmInitializeServiceExtension
+                       0x108c  3  PsmMemoryLimitNotification */
+  return;
+}
+
+void PsmInitializeServiceExtension(void)
+
+{
+                    /* 0x108c  1  PsmApplicationStateNotification
+                       0x108c  2  PsmInitializeServiceExtension
+                       0x108c  3  PsmMemoryLimitNotification */
+  return;
+}
+
+void PsmMemoryLimitNotification(void)
+
+{
+                    /* 0x108c  1  PsmApplicationStateNotification
+                       0x108c  2  PsmInitializeServiceExtension
+                       0x108c  3  PsmMemoryLimitNotification */
+  return;
+}
