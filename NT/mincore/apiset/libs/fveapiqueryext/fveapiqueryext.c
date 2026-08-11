53cad6ab2398612c950f2dfd99adfb9b7c59158c
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 16:38:54 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/fveapiqueryext/fveapiqueryext.c
@@ -0,0 +1,40 @@
+
+int __cdecl FveGetStatusW(void)
+
+{
+                    /* 0x1088  1  FveGetStatusW
+                       0x1088  2  FveSysCloseVolume
+                       0x1088  3  FveSysOpenVolumeW
+                       0x1088  4  FveSysSetUserFlags */
+  return 0;
+}
+
+int __cdecl FveSysCloseVolume(void)
+
+{
+                    /* 0x1088  1  FveGetStatusW
+                       0x1088  2  FveSysCloseVolume
+                       0x1088  3  FveSysOpenVolumeW
+                       0x1088  4  FveSysSetUserFlags */
+  return 0;
+}
+
+int __cdecl FveSysOpenVolumeW(void)
+
+{
+                    /* 0x1088  1  FveGetStatusW
+                       0x1088  2  FveSysCloseVolume
+                       0x1088  3  FveSysOpenVolumeW
+                       0x1088  4  FveSysSetUserFlags */
+  return 0;
+}
+
+int __cdecl FveSysSetUserFlags(void)
+
+{
+                    /* 0x1088  1  FveGetStatusW
+                       0x1088  2  FveSysCloseVolume
+                       0x1088  3  FveSysOpenVolumeW
+                       0x1088  4  FveSysSetUserFlags */
+  return 0;
+}
