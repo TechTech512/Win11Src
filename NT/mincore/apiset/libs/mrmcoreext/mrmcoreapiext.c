2fb781f7d9b09a99d6350d7bba466c06d1428489
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-10 20:16:00 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/mrmcoreext/mrmcoreapiext.c
@@ -0,0 +1,27 @@
+
+int __cdecl GetStringValueForManifestField(void)
+
+{
+                    /* 0x1090  1  GetStringValueForManifestField
+                       0x1090  2  ResourceManagerQueueGetString
+                       0x1090  3  ResourceManagerQueueIsResourceReference */
+  return 0;
+}
+
+int __cdecl ResourceManagerQueueGetString(void)
+
+{
+                    /* 0x1090  1  GetStringValueForManifestField
+                       0x1090  2  ResourceManagerQueueGetString
+                       0x1090  3  ResourceManagerQueueIsResourceReference */
+  return 0;
+}
+
+int __cdecl ResourceManagerQueueIsResourceReference(void)
+
+{
+                    /* 0x1090  1  GetStringValueForManifestField
+                       0x1090  2  ResourceManagerQueueGetString
+                       0x1090  3  ResourceManagerQueueIsResourceReference */
+  return 0;
+}
