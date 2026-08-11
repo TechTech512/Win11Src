55a8a36a517303ea378e0ce7df35c4d40f04722a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 10:43:57 -0400

    Add files via upload

---
+++ NT/minkernel/apiset/libs/datetimeext/datetimeapiext.c
@@ -0,0 +1,40 @@
+
+int __cdecl GetDateFormatAWorker(void)
+
+{
+                    /* 0x108c  1  GetDateFormatAWorker
+                       0x108c  2  GetDateFormatWWorker
+                       0x108c  3  GetTimeFormatAWorker
+                       0x108c  4  GetTimeFormatWWorker */
+  return 0;
+}
+
+int __cdecl GetDateFormatWWorker(void)
+
+{
+                    /* 0x108c  1  GetDateFormatAWorker
+                       0x108c  2  GetDateFormatWWorker
+                       0x108c  3  GetTimeFormatAWorker
+                       0x108c  4  GetTimeFormatWWorker */
+  return 0;
+}
+
+int __cdecl GetTimeFormatAWorker(void)
+
+{
+                    /* 0x108c  1  GetDateFormatAWorker
+                       0x108c  2  GetDateFormatWWorker
+                       0x108c  3  GetTimeFormatAWorker
+                       0x108c  4  GetTimeFormatWWorker */
+  return 0;
+}
+
+int __cdecl GetTimeFormatWWorker(void)
+
+{
+                    /* 0x108c  1  GetDateFormatAWorker
+                       0x108c  2  GetDateFormatWWorker
+                       0x108c  3  GetTimeFormatAWorker
+                       0x108c  4  GetTimeFormatWWorker */
+  return 0;
+}
