ddcd55ab33e52a204c087f73f3247e37594c7aa7
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 15:51:25 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/metafileext/metafileext.c
@@ -0,0 +1,72 @@
+
+int __cdecl DeleteEnhMetaFile(void)
+
+{
+                    /* 0x1088  1  DeleteEnhMetaFile
+                       0x1088  2  DeleteMetaFile
+                       0x1088  3  GetEnhMetaFileBits
+                       0x1088  4  GetMetaFileBitsEx
+                       0x1088  5  SetEnhMetaFileBits
+                       0x1088  6  SetMetaFileBitsEx */
+  return 0;
+}
+
+int __cdecl DeleteMetaFile(void)
+
+{
+                    /* 0x1088  1  DeleteEnhMetaFile
+                       0x1088  2  DeleteMetaFile
+                       0x1088  3  GetEnhMetaFileBits
+                       0x1088  4  GetMetaFileBitsEx
+                       0x1088  5  SetEnhMetaFileBits
+                       0x1088  6  SetMetaFileBitsEx */
+  return 0;
+}
+
+int __cdecl GetEnhMetaFileBits(void)
+
+{
+                    /* 0x1088  1  DeleteEnhMetaFile
+                       0x1088  2  DeleteMetaFile
+                       0x1088  3  GetEnhMetaFileBits
+                       0x1088  4  GetMetaFileBitsEx
+                       0x1088  5  SetEnhMetaFileBits
+                       0x1088  6  SetMetaFileBitsEx */
+  return 0;
+}
+
+int __cdecl GetMetaFileBitsEx(void)
+
+{
+                    /* 0x1088  1  DeleteEnhMetaFile
+                       0x1088  2  DeleteMetaFile
+                       0x1088  3  GetEnhMetaFileBits
+                       0x1088  4  GetMetaFileBitsEx
+                       0x1088  5  SetEnhMetaFileBits
+                       0x1088  6  SetMetaFileBitsEx */
+  return 0;
+}
+
+int __cdecl SetEnhMetaFileBits(void)
+
+{
+                    /* 0x1088  1  DeleteEnhMetaFile
+                       0x1088  2  DeleteMetaFile
+                       0x1088  3  GetEnhMetaFileBits
+                       0x1088  4  GetMetaFileBitsEx
+                       0x1088  5  SetEnhMetaFileBits
+                       0x1088  6  SetMetaFileBitsEx */
+  return 0;
+}
+
+int __cdecl SetMetaFileBitsEx(void)
+
+{
+                    /* 0x1088  1  DeleteEnhMetaFile
+                       0x1088  2  DeleteMetaFile
+                       0x1088  3  GetEnhMetaFileBits
+                       0x1088  4  GetMetaFileBitsEx
+                       0x1088  5  SetEnhMetaFileBits
+                       0x1088  6  SetMetaFileBitsEx */
+  return 0;
+}
