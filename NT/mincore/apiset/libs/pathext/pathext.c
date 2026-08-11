57586bb8ae8259ec5553b8d9d7d6022cb96a7bdd
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 15:12:51 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/pathext/pathext.c
@@ -0,0 +1,55 @@
+
+int __cdecl BeginPath(void)
+
+{
+                    /* 0x1084  1  BeginPath
+                       0x1084  2  CloseFigure
+                       0x1084  3  EndPath
+                       0x1084  4  FillPath
+                       0x1084  5  PathToRegion */
+  return 0;
+}
+
+int __cdecl CloseFigure(void)
+
+{
+                    /* 0x1084  1  BeginPath
+                       0x1084  2  CloseFigure
+                       0x1084  3  EndPath
+                       0x1084  4  FillPath
+                       0x1084  5  PathToRegion */
+  return 0;
+}
+
+int __cdecl EndPath(void)
+
+{
+                    /* 0x1084  1  BeginPath
+                       0x1084  2  CloseFigure
+                       0x1084  3  EndPath
+                       0x1084  4  FillPath
+                       0x1084  5  PathToRegion */
+  return 0;
+}
+
+int __cdecl FillPath(void)
+
+{
+                    /* 0x1084  1  BeginPath
+                       0x1084  2  CloseFigure
+                       0x1084  3  EndPath
+                       0x1084  4  FillPath
+                       0x1084  5  PathToRegion */
+  return 0;
+}
+
+int __cdecl PathToRegion(void)
+
+{
+                    /* 0x1084  1  BeginPath
+                       0x1084  2  CloseFigure
+                       0x1084  3  EndPath
+                       0x1084  4  FillPath
+                       0x1084  5  PathToRegion */
+  return 0;
+}
