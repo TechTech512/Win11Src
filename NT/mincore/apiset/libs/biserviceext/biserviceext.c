ec9ba88d7b80752b5f5a2f3d1d2dd85c63c1d737
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 18:12:59 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/biserviceext/biserviceext.c
@@ -0,0 +1,14 @@
+
+int __cdecl BiEvaluateActivationAction(void)
+
+{
+                    /* 0x108c  1  BiEvaluateActivationAction */
+  return 0;
+}
+
+void BiTaskStateChangeNotification(void)
+
+{
+                    /* 0x1090  2  BiTaskStateChangeNotification */
+  return;
+}
