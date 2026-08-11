58fb9e6c567c94f2d82f13571bd11b7562f89d2c
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-11 14:21:15 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/renderext/renderext.c
@@ -0,0 +1,27 @@
+
+int __cdecl BeginGdiRendering(void)
+
+{
+                    /* 0x1088  1  BeginGdiRendering
+                       0x1088  2  CreateBitmapFromDxSurface2
+                       0x1088  3  EndGdiRendering */
+  return 0;
+}
+
+int __cdecl CreateBitmapFromDxSurface2(void)
+
+{
+                    /* 0x1088  1  BeginGdiRendering
+                       0x1088  2  CreateBitmapFromDxSurface2
+                       0x1088  3  EndGdiRendering */
+  return 0;
+}
+
+int __cdecl EndGdiRendering(void)
+
+{
+                    /* 0x1088  1  BeginGdiRendering
+                       0x1088  2  CreateBitmapFromDxSurface2
+                       0x1088  3  EndGdiRendering */
+  return 0;
+}
