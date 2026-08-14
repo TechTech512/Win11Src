f49e1eda42e372c28d310c47ce2d0bfa5117c19f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 19:56:59 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32msiext/advapi32msiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl CommandLineFromMsiDescriptor(void)
+
+{
+                    /* 0x1088  1  CommandLineFromMsiDescriptor
+                       0x1088  2  InstallApplication */
+  return 0;
+}
+
+int __cdecl InstallApplication(void)
+
+{
+                    /* 0x1088  1  CommandLineFromMsiDescriptor
+                       0x1088  2  InstallApplication */
+  return 0;
+}
