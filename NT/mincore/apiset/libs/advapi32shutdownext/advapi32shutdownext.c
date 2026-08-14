6cd5306034d5ec0ee7a2fa6f6089cc33f5096af9
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 19:35:09 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32shutdownext/advapi32shutdownext.c
@@ -0,0 +1,40 @@
+
+int __cdecl AbortSystemShutdownW(void)
+
+{
+                    /* 0x108c  1  AbortSystemShutdownW
+                       0x108c  2  CheckForHiberboot
+                       0x108c  3  InitiateShutdownW
+                       0x108c  4  InitiateSystemShutdownExW */
+  return 0;
+}
+
+int __cdecl CheckForHiberboot(void)
+
+{
+                    /* 0x108c  1  AbortSystemShutdownW
+                       0x108c  2  CheckForHiberboot
+                       0x108c  3  InitiateShutdownW
+                       0x108c  4  InitiateSystemShutdownExW */
+  return 0;
+}
+
+int __cdecl InitiateShutdownW(void)
+
+{
+                    /* 0x108c  1  AbortSystemShutdownW
+                       0x108c  2  CheckForHiberboot
+                       0x108c  3  InitiateShutdownW
+                       0x108c  4  InitiateSystemShutdownExW */
+  return 0;
+}
+
+int __cdecl InitiateSystemShutdownExW(void)
+
+{
+                    /* 0x108c  1  AbortSystemShutdownW
+                       0x108c  2  CheckForHiberboot
+                       0x108c  3  InitiateShutdownW
+                       0x108c  4  InitiateSystemShutdownExW */
+  return 0;
+}
