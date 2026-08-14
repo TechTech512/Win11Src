23491f1bffc179ca286e5938d93ba934b2df5d92
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 19:51:17 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32ntmartaext/advapi32ntmartaext.c
@@ -0,0 +1,27 @@
+
+int __cdecl GetSecurityInfo(void)
+
+{
+                    /* 0x108c  1  GetSecurityInfo
+                       0x108c  2  SetSecurityInfo
+                       0x108c  3  TreeResetNamedSecurityInfoW */
+  return 0;
+}
+
+int __cdecl SetSecurityInfo(void)
+
+{
+                    /* 0x108c  1  GetSecurityInfo
+                       0x108c  2  SetSecurityInfo
+                       0x108c  3  TreeResetNamedSecurityInfoW */
+  return 0;
+}
+
+int __cdecl TreeResetNamedSecurityInfoW(void)
+
+{
+                    /* 0x108c  1  GetSecurityInfo
+                       0x108c  2  SetSecurityInfo
+                       0x108c  3  TreeResetNamedSecurityInfoW */
+  return 0;
+}
