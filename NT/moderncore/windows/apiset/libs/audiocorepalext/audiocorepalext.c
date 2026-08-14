4cab8ef093e00825e28a91d2947359d962ad6adc
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 18:51:08 -0400

    Add files via upload

---
+++ NT/moderncore/windows/apiset/libs/audiocorepalext/audiocorepalext.c
@@ -0,0 +1,16 @@
+
+int __cdecl IsMuxSelectionSupported(void)
+
+{
+                    /* 0x1088  1  IsMuxSelectionSupported
+                       0x1088  2  IsProtectedAudioOffloadSupported */
+  return 0;
+}
+
+int __cdecl IsProtectedAudioOffloadSupported(void)
+
+{
+                    /* 0x1088  1  IsMuxSelectionSupported
+                       0x1088  2  IsProtectedAudioOffloadSupported */
+  return 0;
+}
