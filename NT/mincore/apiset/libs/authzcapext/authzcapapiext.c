dc7a1631f2d65261652c66c7224fdcea6b1efe8f
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 18:13:37 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/authzcapext/authzcapapiext.c
@@ -0,0 +1,16 @@
+
+int __cdecl GetCentralAccessPoliciesByCapID(void)
+
+{
+                    /* 0x1090  1  GetCentralAccessPoliciesByCapID
+                       0x1090  2  GetCentralAccessPoliciesByDN */
+  return 0;
+}
+
+int __cdecl GetCentralAccessPoliciesByDN(void)
+
+{
+                    /* 0x1090  1  GetCentralAccessPoliciesByCapID
+                       0x1090  2  GetCentralAccessPoliciesByDN */
+  return 0;
+}
