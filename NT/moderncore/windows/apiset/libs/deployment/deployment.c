f060ef9ca4a74ed2730c4787dd8665abc7f4b10a
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 19:27:38 -0400

    Add files via upload

---
+++ NT/moderncore/windows/apiset/libs/deployment/deployment.c
@@ -0,0 +1,187 @@
+
+int __cdecl ApplyStrongProtection(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl CreateAppxDataProfile(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl CreateAppxInstallProfile(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl DeleteAppxDataProfile(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl DeleteAppxInstallProfile(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl DeprovisionMarketplaceApplication(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl GetUserTempPath(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl GetWPCapabilities(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl GetWPCapabilitiesCount(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl IsProvisioningAllowed(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
+
+int __cdecl ProvisionMarketplaceApplication(void)
+
+{
+                    /* 0x1090  1  ApplyStrongProtection
+                       0x1090  2  CreateAppxDataProfile
+                       0x1090  3  CreateAppxInstallProfile
+                       0x1090  4  DeleteAppxDataProfile
+                       0x1090  5  DeleteAppxInstallProfile
+                       0x1090  6  DeprovisionMarketplaceApplication
+                       0x1090  7  GetUserTempPath
+                       0x1090  8  GetWPCapabilities
+                       0x1090  9  GetWPCapabilitiesCount
+                       0x1090  10  IsProvisioningAllowed
+                       0x1090  11  ProvisionMarketplaceApplication */
+  return 0;
+}
