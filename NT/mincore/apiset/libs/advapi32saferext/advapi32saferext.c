d48765b10ee87f99072ea71d7b3d14596bcf45da
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 19:43:13 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32saferext/advapi32saferext.c
@@ -0,0 +1,91 @@
+
+int __cdecl SaferCloseLevel(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
+
+int __cdecl SaferComputeTokenFromLevel(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
+
+int __cdecl SaferCreateLevel(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
+
+int __cdecl SaferIdentifyLevel(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
+
+int __cdecl SaferiCompareTokenLevels(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
+
+int __cdecl SaferiIsExecutableFileType(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
+
+int __cdecl SaferiSearchMatchingHashRules(void)
+
+{
+                    /* 0x108c  1  SaferCloseLevel
+                       0x108c  2  SaferComputeTokenFromLevel
+                       0x108c  3  SaferCreateLevel
+                       0x108c  4  SaferIdentifyLevel
+                       0x108c  5  SaferiCompareTokenLevels
+                       0x108c  6  SaferiIsExecutableFileType
+                       0x108c  7  SaferiSearchMatchingHashRules */
+  return 0;
+}
