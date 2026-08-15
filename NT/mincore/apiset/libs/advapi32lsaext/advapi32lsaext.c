c1f46ceba5958eb1bf5578e0cc6e16256efbaacc
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 20:05:43 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32lsaext/advapi32lsaext.c
@@ -0,0 +1,55 @@
+
+int __cdecl LsaAddPrivilegesToAccount(void)
+
+{
+                    /* 0x1088  1  LsaAddPrivilegesToAccount
+                       0x1088  2  LsaCreateAccount
+                       0x1088  3  LsaGetUserName
+                       0x1088  4  LsaOpenAccount
+                       0x1088  5  LsaSetSystemAccessAccount */
+  return 0;
+}
+
+int __cdecl LsaCreateAccount(void)
+
+{
+                    /* 0x1088  1  LsaAddPrivilegesToAccount
+                       0x1088  2  LsaCreateAccount
+                       0x1088  3  LsaGetUserName
+                       0x1088  4  LsaOpenAccount
+                       0x1088  5  LsaSetSystemAccessAccount */
+  return 0;
+}
+
+int __cdecl LsaGetUserName(void)
+
+{
+                    /* 0x1088  1  LsaAddPrivilegesToAccount
+                       0x1088  2  LsaCreateAccount
+                       0x1088  3  LsaGetUserName
+                       0x1088  4  LsaOpenAccount
+                       0x1088  5  LsaSetSystemAccessAccount */
+  return 0;
+}
+
+int __cdecl LsaOpenAccount(void)
+
+{
+                    /* 0x1088  1  LsaAddPrivilegesToAccount
+                       0x1088  2  LsaCreateAccount
+                       0x1088  3  LsaGetUserName
+                       0x1088  4  LsaOpenAccount
+                       0x1088  5  LsaSetSystemAccessAccount */
+  return 0;
+}
+
+int __cdecl LsaSetSystemAccessAccount(void)
+
+{
+                    /* 0x1088  1  LsaAddPrivilegesToAccount
+                       0x1088  2  LsaCreateAccount
+                       0x1088  3  LsaGetUserName
+                       0x1088  4  LsaOpenAccount
+                       0x1088  5  LsaSetSystemAccessAccount */
+  return 0;
+}
