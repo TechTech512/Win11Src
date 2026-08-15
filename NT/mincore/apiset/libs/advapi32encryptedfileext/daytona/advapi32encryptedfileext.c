af37d9978d41f962809d97183f3a9616d061d5dc
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 20:35:28 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32encryptedfileext/daytona/advapi32encryptedfileext.c
@@ -0,0 +1,34 @@
+
+void CloseEncryptedFileRaw(void)
+
+{
+                    /* 0x1094  1  CloseEncryptedFileRaw */
+  return;
+}
+
+int __cdecl OpenEncryptedFileRawW(void)
+
+{
+                    /* 0x1098  2  OpenEncryptedFileRawW
+                       0x1098  3  ReadEncryptedFileRaw
+                       0x1098  4  WriteEncryptedFileRaw */
+  return 0;
+}
+
+int __cdecl ReadEncryptedFileRaw(void)
+
+{
+                    /* 0x1098  2  OpenEncryptedFileRawW
+                       0x1098  3  ReadEncryptedFileRaw
+                       0x1098  4  WriteEncryptedFileRaw */
+  return 0;
+}
+
+int __cdecl WriteEncryptedFileRaw(void)
+
+{
+                    /* 0x1098  2  OpenEncryptedFileRawW
+                       0x1098  3  ReadEncryptedFileRaw
+                       0x1098  4  WriteEncryptedFileRaw */
+  return 0;
+}
