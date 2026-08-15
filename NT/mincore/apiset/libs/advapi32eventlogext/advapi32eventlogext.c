f5f325eb5f5fa3e7769dca0324fc284c266d6385
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 20:20:50 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32eventlogext/advapi32eventlogext.c
@@ -0,0 +1,55 @@
+
+int __cdecl CloseEventLog(void)
+
+{
+                    /* 0x108c  1  CloseEventLog
+                       0x108c  2  DeregisterEventSource
+                       0x108c  3  OpenEventLogW
+                       0x108c  4  RegisterEventSourceW
+                       0x108c  5  ReportEventW */
+  return 0;
+}
+
+int __cdecl DeregisterEventSource(void)
+
+{
+                    /* 0x108c  1  CloseEventLog
+                       0x108c  2  DeregisterEventSource
+                       0x108c  3  OpenEventLogW
+                       0x108c  4  RegisterEventSourceW
+                       0x108c  5  ReportEventW */
+  return 0;
+}
+
+int __cdecl OpenEventLogW(void)
+
+{
+                    /* 0x108c  1  CloseEventLog
+                       0x108c  2  DeregisterEventSource
+                       0x108c  3  OpenEventLogW
+                       0x108c  4  RegisterEventSourceW
+                       0x108c  5  ReportEventW */
+  return 0;
+}
+
+int __cdecl RegisterEventSourceW(void)
+
+{
+                    /* 0x108c  1  CloseEventLog
+                       0x108c  2  DeregisterEventSource
+                       0x108c  3  OpenEventLogW
+                       0x108c  4  RegisterEventSourceW
+                       0x108c  5  ReportEventW */
+  return 0;
+}
+
+int __cdecl ReportEventW(void)
+
+{
+                    /* 0x108c  1  CloseEventLog
+                       0x108c  2  DeregisterEventSource
+                       0x108c  3  OpenEventLogW
+                       0x108c  4  RegisterEventSourceW
+                       0x108c  5  ReportEventW */
+  return 0;
+}
