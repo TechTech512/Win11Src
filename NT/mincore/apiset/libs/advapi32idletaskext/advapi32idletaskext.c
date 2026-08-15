6752cba6a6ea09dc840422c9963f31388951152b
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 20:13:22 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32idletaskext/advapi32idletaskext.c
@@ -0,0 +1,27 @@
+
+int __cdecl ProcessIdleTasks(void)
+
+{
+                    /* 0x108c  1  ProcessIdleTasks
+                       0x108c  2  RegisterIdleTask
+                       0x108c  3  UnregisterIdleTask */
+  return 0;
+}
+
+int __cdecl RegisterIdleTask(void)
+
+{
+                    /* 0x108c  1  ProcessIdleTasks
+                       0x108c  2  RegisterIdleTask
+                       0x108c  3  UnregisterIdleTask */
+  return 0;
+}
+
+int __cdecl UnregisterIdleTask(void)
+
+{
+                    /* 0x108c  1  ProcessIdleTasks
+                       0x108c  2  RegisterIdleTask
+                       0x108c  3  UnregisterIdleTask */
+  return 0;
+}
