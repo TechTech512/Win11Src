7230b59cd35dfdb29b7914c259bc2c586b41b938
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-14 21:20:17 -0400

    Add files via upload

---
+++ NT/mincore/apiset/libs/advapi32authext/advapi32authext.c
@@ -0,0 +1,16 @@
+
+int __cdecl LookupPrivilegeValueA(void)
+
+{
+                    /* 0x1088  1  LookupPrivilegeValueA
+                       0x1088  2  LookupPrivilegeValueW */
+  return 0;
+}
+
+int __cdecl LookupPrivilegeValueW(void)
+
+{
+                    /* 0x1088  1  LookupPrivilegeValueA
+                       0x1088  2  LookupPrivilegeValueW */
+  return 0;
+}
