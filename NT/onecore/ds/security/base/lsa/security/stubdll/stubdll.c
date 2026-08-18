27046e186182fe90a6aad427f3dd305f02f3b852
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-08-17 21:54:41 -0400

    Add stub functions for secur32.DLL
    Implemented stub functions for security-related calls to secur32.DLL.

---
+++ NT/onecore/ds/security/base/lsa/security/stubdll/stubdll.c
@@ -0,0 +1,53 @@
+//+---------------------------------------------------------------------------
+//
+//  Microsoft Windows
+//  Copyright (C) Microsoft Corporation, 1992 - 1995.
+//
+//  File:       stubdll.c
+//
+//  Contents:   Stub calls to secur32.DLL
+//
+//  Classes:
+//
+//  Functions:
+//
+//  History:    10-05-96   RichardW   Created
+//
+//----------------------------------------------------------------------------
+
+void AcceptSecurityContext(void){}
+void AcquireCredentialsHandleA(void){}
+void AcquireCredentialsHandleW(void){}
+void AddSecurityPackageA(void){}
+void AddSecurityPackageW(void){}
+void ApplyControlToken(void){}
+void CompleteAuthToken(void){}
+void DecryptMessage(void){}
+void DeleteSecurityContext(void){}
+void DeleteSecurityPackageA(void){}
+void DeleteSecurityPackageW(void){}
+void EncryptMessage(void){}
+void EnumerateSecurityPackagesA(void){}
+void EnumerateSecurityPackagesW(void){}
+void ExportSecurityContext(void){}
+void FreeContextBuffer(void){}
+void FreeCredentialsHandle(void){}
+void ImpersonateSecurityContext(void){}
+void ImportSecurityContextA(void){}
+void ImportSecurityContextW(void){}
+void InitializeSecurityContextA(void){}
+void InitializeSecurityContextW(void){}
+void InitSecurityInterfaceA(void){}
+void InitSecurityInterfaceW(void){}
+void MakeSignature(void){}
+void QueryContextAttributesA(void){}
+void QueryContextAttributesW(void){}
+void QueryCredentialsAttributesA(void){}
+void QueryCredentialsAttributesW(void){}
+void QuerySecurityContextToken(void){}
+void QuerySecurityPackageInfoA(void){}
+void QuerySecurityPackageInfoW(void){}
+void RevertSecurityContext(void){}
+void SealMessage(void){}
+void UnsealMessage(void){}
+void VerifySignature(void){}
