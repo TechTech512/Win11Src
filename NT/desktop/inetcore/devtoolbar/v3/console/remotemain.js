50c7f381fb6abb2a8f227da799aa0b00d122638c
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-09-11 20:53:56 -0400

    Add files via upload

---
+++ NT/desktop/inetcore/devtoolbar/v3/console/remotemain.js
@@ -0,0 +1,3 @@
+//! Copyright (C) Microsoft. All rights reserved.
+var remoteHelpers,consoleRemoteCode;(function(){"use strict";remoteHelpers=new Common.RemoteHelpers(new F12.Tools.Console.MessageHandlers);consoleRemoteCode=new F12.Tools.Console.RemoteCode;consoleRemoteCode.initialize()})()
+//# sourceMappingURL=remoteMain.min.js.map
