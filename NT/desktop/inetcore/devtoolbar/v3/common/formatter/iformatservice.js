760b164cd9323cdfce29b7864d31d3e1eedfca81
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-09-11 21:23:35 -0400

    Add files via upload

---
+++ NT/desktop/inetcore/devtoolbar/v3/common/formatter/iformatservice.js
@@ -0,0 +1,3 @@
+//! Copyright (C) Microsoft. All rights reserved.
+var Common;(function(n){var t;(function(n){"use strict";(function(n){n[n.Original=0]="Original";n[n.Sourcemap=1]="Sourcemap";n[n.PrettyPrint=2]="PrettyPrint"})(n.SourceType||(n.SourceType={}));var t=n.SourceType})(t=n.FormatService||(n.FormatService={}))})(Common||(Common={}))
+//# sourceMappingURL=IFormatService.min.js.map
