40ca98e5bd77ff36f94730388b927292ca8b9293
RoguePluto82523 <74076300+TechTech512@users.noreply.github.com>
2026-09-11 20:52:18 -0400

    Add files via upload

---
+++ NT/desktop/inetcore/devtoolbar/v3/chooser/app.js
@@ -0,0 +1,3 @@
+//! Copyright (C) Microsoft. All rights reserved.
+var F12;(function(n){var t;(function(n){var t;(function(n){"use strict";var t=function(){function t(){}return t.prototype.main=function(){var t=new n.Chooser;t.checkLaunchParameters()},t}();n.ChooserApp=t;n.app=new t})(t=n.Chooser||(n.Chooser={}))})(t=n.Tools||(n.Tools={}))})(F12||(F12={}))
+//# sourceMappingURL=app.min.js.map
